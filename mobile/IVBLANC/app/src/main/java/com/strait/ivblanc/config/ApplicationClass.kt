faac1c195e77a047d0800dce687d4bb22a9255b9
김민수 <ms001118@naver.com>
2022-01-17 17:31:31 +0900

    MO-232 retrofit client 생성, 적용

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/config/ApplicationClass.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/config/ApplicationClass.kt
@@ -1,12 +1,18 @@
 package com.strait.ivblanc.config
 
 import android.app.Application
+import com.ssafy.template.config.ReceivedCookiesInterceptor
+import com.ssafy.template.config.XAccessTokenInterceptor
 import com.strait.ivblanc.util.SharedPreferencesUtil
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApplicationClass: Application() {
     val BASE_URL = "http://119.56.162.61:9999"
+    val TIME_OUT = 5000L
 
     companion object {
 
@@ -26,9 +32,21 @@ class ApplicationClass: Application() {
         initRetrofit()
     }
 
+    // 레트로핏 인스턴스를 생성하고, 레트로핏에 각종 설정값들을 지정해줍니다.
+    // 연결 타임아웃시간은 5초로 지정이 되어있고, HttpLoggingInterceptor를 붙여서 어떤 요청이 나가고 들어오는지를 보여줍니다.
     fun initRetrofit() {
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .readTimeout(TIME_OUT, TimeUnit.MILLISECONDS)
+            .connectTimeout(TIME_OUT, TimeUnit.MILLISECONDS)
+            // 로그캣에 okhttp.OkHttpClient로 검색하면 http 통신 내용을 보여줍니다.
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addNetworkInterceptor(XAccessTokenInterceptor()) // JWT 자동 헤더 전송
+            .addInterceptor(AddCookiesInterceptor())  //쿠키 전송
+            .addInterceptor(ReceivedCookiesInterceptor()) //쿠키 추출
+            .build()
 
         sRetrofit = Retrofit.Builder().baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
