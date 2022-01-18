22301bf3b7e7e231eb9701237b18f45134ddcac4
김민수 <ms001118@naver.com>
2022-01-18 13:09:11 +0900

    MO-99 로그인 액티비티 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.strait.ivblanc.src.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.strait.ivblanc.R
+import com.strait.ivblanc.config.BaseActivity
+import com.strait.ivblanc.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
