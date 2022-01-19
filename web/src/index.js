fc2ec76cf14f4136268aac78842614ab6efb8554
김현수 <hskim9706@gmail.com>
2022-01-19 05:40:20 +0000

    Revert "Merge branch 'feature-FE-51' into 'develop'"
    This reverts merge request !3

--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import SignIn from './pages/SignIn/Signin';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <SignIn />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
