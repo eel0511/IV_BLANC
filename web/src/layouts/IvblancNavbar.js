3ed3e03180c6ff91f95d38bafd9cb031b5c037bd
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-01-19 13:43:00 +0900

    FE-89 내옷장 사이드바 초기세팅

---
+++ web/src/layouts/IvblancNavbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export default function IvblancNavbar() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <aside class="navbar-brand" href="/">
+          Home
+        </aside>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/mycloset">
+                내 옷장
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/styling">
+                스타일링
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/history">
+                히스토리
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/friends">
+                친구 옷장
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ web/src/pages/MyCloset.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MyCloset() {
+  return (
+    <>
+      <h1>My Closet</h1>
+    </>
+  );
+}
