1153f35b822f002b2255e81485b27a3b22bf3bdc
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-01-19 17:24:17 +0900

    FE-89 내 옷장 옷 전체 조회 임시저장

--- web/src/pages/MyCloset.js
+++ web/src/pages/MyCloset.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import MyClosetClothes from "../layouts/MyCloset/MyClosetClothes";
 import MyClosetSidebar from "../layouts/MyCloset/MyClosetSidebar";
 
 export default function MyCloset() {
+  const [myClothes, setMyClothes] = useState([]);
+
+  const testAxios = () => {
+    axios(
+      {
+        url: 'api/clothes/all',
+        method: 'get',
+        baseURL: 'http://119.56.162.61:9999',
+        data: {
+          "page": 1,
+          "userId": 1
+        }
+      }
+    ).then((response) => {
+      console.log(response.data)
+    });
+  };
+
+  // useEffect(() => {
+  //   testAxios();
+  // }, []);
+
   return (
     <>
       <h1>My Closet</h1>
@@ -12,7 +35,7 @@ export default function MyCloset() {
             <MyClosetSidebar />
           </div>
           <div className="col-10">
-            <MyClosetClothes />
+            <MyClosetClothes myClothes={myClothes}/>
           </div>
         </div>
       </div>
