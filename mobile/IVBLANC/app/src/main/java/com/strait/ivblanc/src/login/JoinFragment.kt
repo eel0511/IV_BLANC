c5500cfffc42f230176824453f8ae4acdcbdb244
김민수 <ms001118@naver.com>
2022-01-19 15:59:46 +0900

    MO-61 유효성 검사에 따른 ui 처리

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/login/JoinFragment.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/login/JoinFragment.kt
@@ -1,15 +1,18 @@
 package com.strait.ivblanc.src.login
 
-import android.app.StatusBarManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.strait.ivblanc.R
 import com.strait.ivblanc.config.BaseFragment
 import com.strait.ivblanc.databinding.FragmentJoinBinding
+import com.strait.ivblanc.util.InputValidUtil
 
 class JoinFragment : BaseFragment<FragmentJoinBinding>(FragmentJoinBinding::bind, R.layout.fragment_join) {
     override fun onCreateView(
@@ -19,18 +22,121 @@ class JoinFragment : BaseFragment<FragmentJoinBinding>(FragmentJoinBinding::bind
     ): View? {
         val parent = super.onCreateView(inflater, container, savedInstanceState)
         val constraintLayout = parent!!.findViewById<ConstraintLayout>(R.id.constaraintLayout_joinF)
-
-        var result = 0
-
-        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
-
-        if (resourceId > 0) {
-
-            result = resources.getDimensionPixelSize(resourceId)
-
-        }
-        constraintLayout.minHeight = requireActivity().windowManager.defaultDisplay.height - result
-
+        setLayoutWithoutStatusBarHeight(constraintLayout)
         return parent
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    fun initView() {
+        binding.buttonJoinFJoin.setOnClickListener {
+            if (checkInputForm()) {
+                //todo: 회원가입 진행
+            }
+        }
+
+        // todo: 리팩터링 해야함
+        binding.editTextJoinFEmail.addTextChangedListener {
+            val input = it.toString()
+            if (InputValidUtil.isValidEmail(input)) {
+                binding.textInputLayoutJoinFEmail.error = null
+            }
+        }
+
+        binding.editTextJoinFPassword.addTextChangedListener {
+            val input = it.toString()
+            if (InputValidUtil.isValidPassword(input)) {
+                binding.textInputLayoutJoinFPassword.error = null
+            }
+        }
+
+        binding.editTextJoinFPasswordcheck.addTextChangedListener {
+            val input = it.toString()
+            val originPassword = binding.editTextJoinFPassword.text.toString()
+            if (InputValidUtil.isValidPassword(input) && input == originPassword) {
+                binding.textInputLayoutJoinFPasswordcheck.error = null
+            }
+        }
+
+        binding.editTextJoinFName.addTextChangedListener {
+            val input = it.toString()
+            if (InputValidUtil.isValidName(input)) {
+                binding.textInputLayoutJoinFName.error = null
+            }
+        }
+
+        binding.editTextJoinFPhone.addTextChangedListener {
+            val input = it.toString()
+            if (InputValidUtil.isValidPhoneNumber(input)) {
+                binding.textInputLayoutJoinFPhone.error = null
+            }
+        }
+
+        binding.editTextJoinFBirthday.addTextChangedListener {
+            val input = it.toString()
+            if (InputValidUtil.isValidBirthDay(input)) {
+                binding.textInputLayoutJoinFBirthday.error = null
+            }
+        }
+    }
+
+    fun checkInputForm(): Boolean {
+        var result = 1
+        val email = binding.editTextJoinFEmail.text.toString()
+        val password = binding.editTextJoinFPassword.text.toString()
+        val passwordCheck = binding.editTextJoinFPasswordcheck.text.toString()
+        val name = binding.editTextJoinFName.text.toString()
+        val birthDay = binding.editTextJoinFBirthday.text.toString()
+        val phoneNumber = binding.editTextJoinFPhone.text.toString()
+        val gendersCheckedId = binding.radioGroupJoinFGenders.checkedRadioButtonId // if not selected any button: -1
+
+        if(!InputValidUtil.isValidEmail(email)) {
+            result *= 0
+            binding.textInputLayoutJoinFEmail.error = resources.getText(R.string.emailErrorMessage)
+        }
+        if(!InputValidUtil.isValidPassword(password) || !InputValidUtil.isValidPassword(passwordCheck)) {
+            result *= 0
+            binding.textInputLayoutJoinFPassword.error = resources.getText(R.string.passwordErrorMessage)
+        }
+        if(!InputValidUtil.isValidPassword(passwordCheck)) {
+            result *= 0
+            binding.textInputLayoutJoinFPasswordcheck.error = resources.getText(R.string.passwordErrorMessage)
+        }
+        if(password != passwordCheck) {
+            result *= 0
+            binding.textInputLayoutJoinFPasswordcheck.error = resources.getText(R.string.passwordCheckErrorMessage)
+        }
+        if(!InputValidUtil.isValidName(name)) {
+            result *= 0
+            binding.textInputLayoutJoinFName.error = resources.getText(R.string.nameErrorMessage)
+        }
+        if(!InputValidUtil.isValidBirthDay(birthDay)) {
+            result *= 0
+            binding.textInputLayoutJoinFBirthday.error = resources.getText(R.string.birthdayErrorMessage)
+        }
+        if(!InputValidUtil.isValidPhoneNumber(phoneNumber)) {
+            result *= 0
+            binding.textInputLayoutJoinFPhone.error = resources.getText(R.string.phoneNumberErrorMessage)
+        }
+        if(gendersCheckedId == -1) {
+            result *= 0
+        }
+
+        return when(result) {
+            1 -> true
+            else -> false
+        }
+    }
+
+    fun setLayoutWithoutStatusBarHeight(layout: ConstraintLayout) {
+        var result = 0
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+        layout.minHeight = requireActivity().resources.displayMetrics.heightPixels - result
     }
 }
