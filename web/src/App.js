0f7e382c85265ca8c46f66c5e0078732d2d9648e
이인섭 <inseob135@gmail.com>
2022-01-19 17:34:04 +0900

    FE-52 로그인, 인증 라우터 추가

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn/Signin';
+import KakaoAuth from './components/login/kakaoOAuth';
+import NaverAuth from './components/login/naverOAuth';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<SignIn />}></Route>
+          <Route path='/oauth/kakao/callback' exact element={<KakaoAuth />}></Route>
+          <Route path='/oauth/naver/callback' exact element={<NaverAuth />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
