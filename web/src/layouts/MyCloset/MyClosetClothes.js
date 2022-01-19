3ebf21a6944e7f4f12cadc5e27768a824c361f69
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-01-19 23:30:52 +0900

    FE-29 들여쓰기 수정

--- web/src/layouts/MyCloset/MyClosetClothes.js
+++ web/src/layouts/MyCloset/MyClosetClothes.js
@@ -8,11 +8,11 @@ export default function MyClosetClothes({ clothesDatas }) {
     <h1>옷들 나타내는 곳</h1>
     <div className="container-fluid">
       <div className="row">
-      {clothesDatas.map((clothesData) => (
-        <div className="col-4 mt-3" key={clothesData.clothesId}>
-          <MyClosetClothesItem clothesData={clothesData} />
-        </div>
-      ))}
+        {clothesDatas.map((clothesData) => (
+          <div className="col-4 mt-3" key={clothesData.clothesId}>
+            <MyClosetClothesItem clothesData={clothesData} />
+          </div>
+        ))}
       </div>
     </div>
     </>
