7bf891b4d4c83ca2548d078014a3f14a51631347
김민수 <ms001118@naver.com>
2022-01-19 17:23:24 +0900

    MO-61 생년월일 유효성 체크 로직 변경

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/util/InputValidUtil.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/util/InputValidUtil.kt
@@ -1,5 +1,10 @@
 package com.strait.ivblanc.util
 
+import android.icu.util.LocaleData
+import java.lang.Exception
+import java.time.LocalDate
+import java.util.*
+
 private const val TAG = "InputValidUtil_strait"
 object InputValidUtil {
     val nameRegex = "^[가-힣a-zA-Z]{2,}+$".toRegex()
@@ -33,8 +38,42 @@ object InputValidUtil {
         return false
     }
 
+
+    // todo: 날짜 계산
     fun isValidBirthDay(birthDay: String): Boolean {
-        return birthDay.matches(birthDayRegex)
+        if(birthDay.matches(birthDayRegex)) {
+            val year = birthDay.substring(0, 2).toInt()
+            val month = birthDay.substring(2, 4).toInt()
+            val day = birthDay.substring(4, 6).toInt()
+
+            var convertedYear = if(year >= 0 && year <= Calendar.getInstance().get(Calendar.YEAR) % 100) {
+                2000 + year
+            } else {
+                1900 + year
+            }
+
+            return isValidDay(convertedYear, month, day)
+        }
+        return false
+    }
+
+    fun isValidDay(year: Int, month: Int, day: Int): Boolean {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+        return if (1900 > year || year > currentYear) {
+            false
+        } else if (month < 1 || month > 12) {
+            false
+        } else if (day < 1 || day > 31) {
+            false
+        } else if ((month==4 || month==6 || month==9 || month==11) && day==31) {
+            false
+        } else if (month == 2) {
+            val isLeap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            !(day > 29 || (day == 29 && !isLeap))
+        } else {
+            true
+        }
     }
 
     fun isValidPhoneNumber(phoneNumber: String): Boolean {
