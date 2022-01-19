2544be10d01e0223e7e688ac398d3e5343098ad4
이인섭 <inseob135@gmail.com>
2022-01-19 17:36:57 +0900

    FE-52 카카오 로그인 REST API 버전

---
+++ web/src/components/login/kakaoLoginRest.js
@@ -0,0 +1,14 @@
+export default function AuthSocial() {
+  const CLIENT_ID = 'd94857cc20c52361cc40e9344bc2dceb';
+  const REDIRECT_URI = 'http://localhost:3000/oauth/kakao/callback';
+
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  return (
+    <div className='App'>
+      <a href={KAKAO_AUTH_URL}>
+        <img src={require('../../assets/kakao_logo.png')} alt='카카오 로그인' width='280px' height='auto'></img>
+      </a>
+    </div>
+  );
+}
