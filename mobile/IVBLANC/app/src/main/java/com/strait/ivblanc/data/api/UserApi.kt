527a4e719079779df3bfd66a7918e531313be503
김민수 <ms001118@naver.com>
2022-01-20 17:37:15 +0900

    MO-101 이메일 체크 함수 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/UserApi.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/UserApi.kt
@@ -1,12 +1,18 @@
 package com.strait.ivblanc.data.api
 
 import com.strait.ivblanc.data.model.dto.UserForJoin
+import com.strait.ivblanc.data.model.response.EmailCheckResponse
 import com.strait.ivblanc.data.model.response.JoinResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface UserApi {
     @POST("/api/sign/signup")
     suspend fun join(@Body user: UserForJoin): Response<JoinResponse>
+
+    // TODO: 2022/01/20 이메일 체크 메서드 수정 필요
+    @POST("/api/sign/")
+    suspend fun emailCheck(email: String): Response<EmailCheckResponse>
 }
---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/response/EmailCheckResponse.kt
@@ -0,0 +1,4 @@
+package com.strait.ivblanc.data.model.response
+
+class EmailCheckResponse {
+}
