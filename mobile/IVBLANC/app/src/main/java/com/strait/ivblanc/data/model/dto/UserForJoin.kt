cff176f29e2351899a76543dc19e2e9d8e705140
김민수 <ms001118@naver.com>
2022-01-20 13:04:35 +0900

    MO-101 UserForJoin 패키지 변경

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/dto/UserForJoin.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/dto/UserForJoin.kt
@@ -1,4 +1,4 @@
-package com.strait.ivblanc.dto
+package com.strait.ivblanc.data.model.dto
 
 data class UserForJoin(val email: String, val password: String , val name: String, val gender: Int, val phoneNumber: String, var social: Int = 0) {
 
