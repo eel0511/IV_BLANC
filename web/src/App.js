084531880a2318e376303bbb8b9c55750044f67d
이인섭 <inseob135@gmail.com>
2022-01-20 23:18:26 +0900

    FE-121 회원가입 라우터 추가

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/SignIn/Signin';
 import KakaoAuth from './components/login/kakaoOAuth';
 import NaverAuth from './components/login/naverOAuth';
+import SignUp from './pages/SignUp/SignUp';
 
 import IvblancNavbar from './layouts/IvblancNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,6 +18,7 @@ function App() {
           <Route path='/' exact element={<SignIn />}></Route>
           <Route path='/oauth/kakao/callback' exact element={<KakaoAuth />}></Route>
           <Route path='/oauth/naver/callback' exact element={<NaverAuth />}></Route>
+          <Route path='/signup' exact element={<SignUp />}></Route>
         </Routes>
       </Router>
     </div>
