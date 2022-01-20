7eac36a261ede8b1b6ef3a6fb8213d2f883dbd27
김민수 <ms001118@naver.com>
2022-01-20 14:37:35 +0900

    MO-101 join 함수 변경

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/UserRepository.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/UserRepository.kt
@@ -1,16 +1,30 @@
 package com.strait.ivblanc.data.repository
 
 import com.strait.ivblanc.config.ApplicationClass
-import com.strait.ivblanc.config.BaseResponse
 import com.strait.ivblanc.data.api.UserApi
 import com.strait.ivblanc.data.model.dto.UserForJoin
 import com.strait.ivblanc.data.model.response.JoinResponse
+import com.strait.ivblanc.util.Resource
 import retrofit2.Response
+import java.lang.Exception
 
 class UserRepository {
     var userApi: UserApi = ApplicationClass.sRetrofit.create(UserApi::class.java)
 
-    suspend fun join(user: UserForJoin): Response<JoinResponse> {
-        return userApi.join(user)
+    suspend fun join(user: UserForJoin): Resource<JoinResponse> {
+        return try {
+            val response = userApi.join(user)
+            if(response.isSuccessful) {
+                return if(response.code() == 200) {
+                    Resource.success(response.body()!!)
+                } else {
+                    Resource.error(null, "회원가입에 실패했습니다.")
+                }
+            } else {
+                Resource.error(null, "알 수 없는 오류입니다.")
+            }
+        } catch (e: Exception) {
+            Resource.error(null, "네트워크 연결 상태를 확인해 주세요")
+        }
     }
 }
