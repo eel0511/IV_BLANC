8da295f360be9087e425c706ce500b9367882a70
김민수 <ms001118@naver.com>
2022-01-20 14:39:57 +0900

    MO-101 UserApi 경로 변경

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/UserApi.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/UserApi.kt
@@ -1,6 +1,5 @@
 package com.strait.ivblanc.data.api
 
-import com.strait.ivblanc.config.BaseResponse
 import com.strait.ivblanc.data.model.dto.UserForJoin
 import com.strait.ivblanc.data.model.response.JoinResponse
 import retrofit2.Response
@@ -8,6 +7,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface UserApi {
-    @POST("/api/user/join")
+    @POST("/api/sign/signup")
     suspend fun join(@Body user: UserForJoin): Response<JoinResponse>
 }
