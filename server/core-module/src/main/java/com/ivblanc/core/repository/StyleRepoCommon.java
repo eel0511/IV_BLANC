04ece73d3b6e6e6e34d3bb174b30fb45465b84d2
SuhyeongLee <eel0511@gmail.com>
2022-01-20 11:52:39 +0900

    BE-105 JPA쿼리 효율성 개선(pageable 안먹힘)

--- server/core-module/src/main/java/com/ivblanc/core/repository/StyleRepoCommon.java
+++ server/core-module/src/main/java/com/ivblanc/core/repository/StyleRepoCommon.java
@@ -4,11 +4,13 @@ package com.ivblanc.core.repository;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ivblanc.core.entity.Style;
 
 @Repository
 public interface StyleRepoCommon {
+	@Query("select s from Style s left join fetch  s.styleDetails")
 	List<Style> findAllByUserId(int userId, Pageable pageable);
 }
