a6cef462b71ffa9127026c4a2d45ec76d04c0624
김민수 <ms001118@naver.com>
2022-01-20 17:39:22 +0900

    MO-101 로그인 뷰모델에 이메일 중복 검사 함수 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/viewmodel/LoginViewModel.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/viewmodel/LoginViewModel.kt
@@ -2,7 +2,9 @@ package com.strait.ivblanc.data.model.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.strait.ivblanc.config.BaseResponse
 import com.strait.ivblanc.data.model.dto.UserForJoin
+import com.strait.ivblanc.data.model.response.EmailCheckResponse
 import com.strait.ivblanc.data.model.response.JoinResponse
 import com.strait.ivblanc.data.repository.UserRepository
 import com.strait.ivblanc.util.Resource
@@ -14,10 +16,19 @@ class LoginViewModel: ViewModel() {
     val userRepository = UserRepository()
 
     val joinRequestLiveData = MutableLiveData<Resource<JoinResponse?>>()
+    val emailCheckRequestLiveData = MutableLiveData<Resource<EmailCheckResponse>>()
 
     fun join(user: UserForJoin) = CoroutineScope(Dispatchers.Main).launch {
+        joinRequestLiveData.postValue(Resource.loading(null))
         CoroutineScope(Dispatchers.IO).launch {
             joinRequestLiveData.postValue(userRepository.join(user))
+        }
+    }
+
+    fun emailCheck(email: String) = CoroutineScope(Dispatchers.Main).launch {
+        emailCheckRequestLiveData.postValue(Resource.loading(null))
+        CoroutineScope(Dispatchers.IO).launch {
+            emailCheckRequestLiveData.postValue(userRepository.emailCheck(email))
         }
     }
 }
