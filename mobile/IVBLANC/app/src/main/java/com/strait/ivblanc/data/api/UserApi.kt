aa6ae62c5a59133379e499274c6f64dc60f4f1b5
김민수 <ms001118@naver.com>
2022-01-20 12:53:45 +0900

    MO-101 UserApi 수정

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/UserApi.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/UserApi.kt
@@ -2,11 +2,12 @@ package com.strait.ivblanc.data.api
 
 import com.strait.ivblanc.config.BaseResponse
 import com.strait.ivblanc.data.model.dto.UserForJoin
+import com.strait.ivblanc.data.model.response.JoinResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface UserApi {
     @POST("/api/user/join")
-    suspend fun join(@Body user: UserForJoin): Response<BaseResponse>
+    suspend fun join(@Body user: UserForJoin): Response<JoinResponse>
 }
