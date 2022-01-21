85a7765a32dec16bcf44b18400d49de60f60ff8f
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-01-21 14:18:09 +0900

    FE-90 네브바 라우터 에러수정

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import IvblancNavbar from './layouts/IvblancNavbar';
+import Home from './pages/Home/Home';
 import MyCloset from './pages/MyCloset/MyCloset';
+import MyStyle from './pages/MyStyle/MyStyle';
+import History from './pages/History/History';
+import Friends from './pages/Friends/Friends';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './pages/SignIn/Signin';
 import KakaoAuth from './components/login/kakaoOAuth';
@@ -9,20 +13,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-      <Router>
-        <div className="App">
-          <IvblancNavbar />
-            <Routes>
-              <Route path='/mycloset' exact element={<MyCloset />}></Route>
-              <Route path='/styling' exact></Route>
-              <Route path='/history' exact></Route>
-              <Route path='/friends' exact></Route>
-              <Route path='/signin' exact element={<SignIn />}></Route>
-              <Route path='/oauth/kakao/callback' exact element={<KakaoAuth />}></Route>
-              <Route path='/oauth/naver/callback' exact element={<NaverAuth />}></Route>
-            </Routes>
-        </div>
-      </Router>
+    <Router>
+      <div className='App'>
+        <IvblancNavbar />
+        <Routes>
+          <Route path='/' exact element={<Home />}></Route>
+          <Route path='/mycloset' exact element={<MyCloset />}></Route>
+          <Route path='/mystyle' exact element={<MyStyle />}></Route>
+          <Route path='/history' exact element={<History />}></Route>
+          <Route path='/friends' exact element={<Friends />}></Route>
+          <Route path='/signin' exact element={<SignIn />}></Route>
+          <Route
+            path='/oauth/kakao/callback'
+            exact
+            element={<KakaoAuth />}
+          ></Route>
+          <Route
+            path='/oauth/naver/callback'
+            exact
+            element={<NaverAuth />}
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- web/src/layouts/IvblancNavbar.js
+++ web/src/layouts/IvblancNavbar.js
@@ -3,41 +3,45 @@ import { Link } from 'react-router-dom';
 
 export default function IvblancNavbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <aside className="navbar-brand" href="/">
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <div className='container-fluid'>
+        <Link className='navbar-brand' to='/'>
           Home
-        </aside>
+        </Link>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/mycloset">
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <Link
+                className='nav-link active'
+                aria-current='page'
+                to='/mycloset'
+              >
                 내 옷장
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/styling">
+            <li className='nav-item'>
+              <Link className='nav-link' to='/mystyle'>
                 스타일링
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/history">
+            <li className='nav-item'>
+              <Link className='nav-link' to='/history'>
                 히스토리
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/friends">
+            <li className='nav-item'>
+              <Link className='nav-link' to='/friends'>
                 친구 옷장
               </Link>
             </li>
