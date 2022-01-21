a98f85f5b309cb8abf23e9ef50d885d8165d95e6
이인섭 <inseob135@gmail.com>
2022-01-21 17:42:03 +0900

    FE-81 비밀번호 찾기 폼 생성

---
+++ web/src/pages/FindAccount/FindPassword.js
@@ -0,0 +1,179 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { useState, useEffect, useCallback } from 'react';
+
+function Copyright(props) {
+  return (
+    <Typography variant='body2' color='text.secondary' align='center' {...props}>
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp() {
+  // 이메일, 비밀번호, 비밀번호 확인, 이름, 성별, 나이, 폰 번호 확인
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  // 오류메시지 상태저장
+  const [emailMessage, setEmailMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('');
+
+  // 유효성 검사
+  const [isEmail, setIsEmail] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
+
+  // 이메일 형식 확인
+  const onChangeEmail = useCallback((e) => {
+    const emailRegex = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const emailCurrent = e.target.value;
+    setEmail(emailCurrent);
+
+    if (!emailRegex.test(emailCurrent)) {
+      setEmailMessage('이메일 형식이 틀렸어요! 다시 확인해주세요');
+      setIsEmail(false);
+    } else {
+      setEmailMessage('올바른 이메일 형식이에요 : )');
+      setIsEmail(true);
+    }
+  }, []);
+
+  // 비밀번호
+  const onChangePassword = useCallback((e) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+    const passwordCurrent = e.target.value;
+    setPassword(passwordCurrent);
+
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage('숫자+영문자+특수문자 조합으로 8자리 이상 16자리 이하로 입력해주세요!');
+      setIsPassword(false);
+    } else {
+      setPasswordMessage('안전한 비밀번호에요 : )');
+      setIsPassword(true);
+    }
+  }, []);
+
+  // 비밀번호 확인
+  const onChangePasswordConfirm = useCallback(
+    (e) => {
+      const passwordConfirmCurrent = e.target.value;
+      setPasswordConfirm(passwordConfirmCurrent);
+
+      if (password === passwordConfirmCurrent) {
+        setPasswordConfirmMessage('비밀번호를 똑같이 입력했어요 :)');
+        setIsPasswordConfirm(true);
+      } else {
+        setPasswordConfirmMessage('비밀번호가 틀려요. 다시 확인해주세요.');
+        setIsPasswordConfirm(false);
+      }
+    },
+    [password]
+  );
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    if (email === '') return alert('이메일을 입력해주세요');
+    if (passwordConfirm === '') return alert('비밀번호를 입력해주세요');
+
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+      name: data.get('name'),
+      gender: data.get('gender') === 'male' ? 1 : 2,
+      age: Number(data.get('age')),
+      phoneNum: data.get('phoneNum'),
+    });
+
+    // 백엔드 통신
+    // const router = useRouter();
+
+    // try {
+    //   await axios
+    //     .post('http://119.56.162.61:8888/api/sign/signup', {
+    //       email: data.get('email'),
+    //       password: data.get('password'),
+    //       name: data.get('name'),
+    //       gender: data.get('gender') === 'male' ? 1 : 2,
+    //       age: Number(data.get('age')),
+    //       phone: data.get('phoneNum'),
+    //       social: 1,
+    //     })
+    //     .then((res) => {
+    //       console.log('response:', res.data);
+    //       if (res.status === 200) {
+    //         alert('회원가입 성공!!');
+    //       }
+    //     });
+    // } catch (err) {
+    //   console.error(err);
+    // }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {/* <img src={require('../../assets/logo2.png')} alt='우리로고' width={'300px'}></img> */}
+          <Typography component='h1' variant='h5'>
+            비밀번호 찾기
+          </Typography>
+          <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <TextField required fullWidth id='email' label='이메일' name='email' autoComplete='email' value={email} onChange={onChangeEmail} />
+                {email.length > 0 && <span className={`message ${isEmail ? 'success' : 'error'}`}>{emailMessage}</span>}
+              </Grid>
+            </Grid>
+            <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+              비밀번호 찾기
+            </Button>
+
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={6}>
+                <Link href='/signin' variant='body2'>
+                  로그인
+                </Link>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Link href='/findemail' variant='body2'>
+                  아이디 찾기
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
