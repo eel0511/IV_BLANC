6625d3f103ffb510c4cb108017ec8fc631d6349e
이인섭 <inseob135@gmail.com>
2022-01-21 15:36:41 +0900

    FE-80 css 수정

--- web/src/components/login/ShowEmail.js
+++ web/src/components/login/ShowEmail.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function showEmail(props) {
-  return props.email !== '' ? <h3 marginTop='10px'> {`${props.name}님의 아이디는 ${props.email}입니다.`} </h3> : <h3> {`등록된 아이디가 없습니다.`} </h3>;
+  return props.email !== '' ? <h3> {`${props.name}님의 아이디는 ${props.email}입니다.`} </h3> : <h3> {`등록된 아이디가 없습니다.`} </h3>;
 }
