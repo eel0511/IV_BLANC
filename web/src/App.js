576c436d168f2594d1c220e801d4e86a7394b32d
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-01-21 14:25:50 +0900

    FE-90 라우터 구조화

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,7 @@
 import './App.css';
 import IvblancNavbar from './layouts/IvblancNavbar';
-import Home from './pages/Home/Home';
-import MyCloset from './pages/MyCloset/MyCloset';
-import MyStyle from './pages/MyStyle/MyStyle';
-import History from './pages/History/History';
-import Friends from './pages/Friends/Friends';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignIn from './pages/SignIn/Signin';
-import KakaoAuth from './components/login/kakaoOAuth';
-import NaverAuth from './components/login/naverOAuth';
+import routes from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -17,22 +10,15 @@ function App() {
       <div className='App'>
         <IvblancNavbar />
         <Routes>
-          <Route path='/' exact element={<Home />}></Route>
-          <Route path='/mycloset' exact element={<MyCloset />}></Route>
-          <Route path='/mystyle' exact element={<MyStyle />}></Route>
-          <Route path='/history' exact element={<History />}></Route>
-          <Route path='/friends' exact element={<Friends />}></Route>
-          <Route path='/signin' exact element={<SignIn />}></Route>
-          <Route
-            path='/oauth/kakao/callback'
-            exact
-            element={<KakaoAuth />}
-          ></Route>
-          <Route
-            path='/oauth/naver/callback'
-            exact
-            element={<NaverAuth />}
-          ></Route>
+          {routes.map(route => {
+            return (
+              <Route
+                path={route.path}
+                exact
+                element={<route.component />}
+              ></Route>
+            );
+          })}
         </Routes>
       </div>
     </Router>
