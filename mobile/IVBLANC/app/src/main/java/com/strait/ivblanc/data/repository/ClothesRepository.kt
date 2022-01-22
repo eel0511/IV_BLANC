d0b84714382d8d7cbb92d39b8328169a8d731958
김민수 <ms001118@naver.com>
2022-01-23 00:04:18 +0900

    MO-267 clothes repository 전체 옷 요청 함수 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/ClothesRepository.kt
@@ -0,0 +1,28 @@
+package com.strait.ivblanc.data.repository
+
+import com.strait.ivblanc.config.ApplicationClass
+import com.strait.ivblanc.data.api.ClothesApi
+import com.strait.ivblanc.data.model.response.ClothesResponse
+import com.strait.ivblanc.util.Resource
+import java.lang.Exception
+
+class ClothesRepository {
+    val clothesApi = ApplicationClass.sRetrofit.create(ClothesApi::class.java)
+
+    suspend fun getAllClothes(page: Int): Resource<ClothesResponse> {
+        return try {
+            val response = clothesApi.getAllClothes(page)
+            if(response.isSuccessful) {
+                return if(response.code() == 200 && response.body()!!.output == 1) {
+                    Resource.success(response.body()!!)
+                } else {
+                    Resource.error(response.body(), "옷 데이터를 조회할 수 없습니다.")
+                }
+            } else {
+                Resource.error(null, "알 수 없는 오류입니다.")
+            }
+        } catch (e: Exception) {
+            Resource.error(null, "네트워크 상태를 확인해 주세요.")
+        }
+    }
+}
