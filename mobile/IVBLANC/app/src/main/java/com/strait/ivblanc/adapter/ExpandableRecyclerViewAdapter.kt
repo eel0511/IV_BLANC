87b5dbc6c746d27f18e82cdff58139eb13f4952a
김민수 <ms001118@naver.com>
2022-01-24 00:19:17 +0900

    MO-5 expandable recyclerview adapter 기본 함수 구현

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/ExpandableRecyclerViewAdapter.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/ExpandableRecyclerViewAdapter.kt
@@ -1,20 +1,57 @@
 package com.strait.ivblanc.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.strait.ivblanc.R
+import com.strait.ivblanc.data.model.dto.Clothes
+import com.strait.ivblanc.data.model.dto.PhotoItem
+import java.lang.Exception
 
-class ExpandableRecyclerViewAdapter: RecyclerView.Adapter<ViewHolder>() {
+class ExpandableRecyclerViewAdapter<T>(val context: Context): RecyclerView.Adapter<ExpandableRecyclerViewAdapter<T>.ViewHolder>() {
+    companion object {
+        const val HEADER = 0
+        const val CHILD = 1
+    }
+
+    val data = arrayListOf<PhotoItem<T>>()
     lateinit var itemClickListener: ItemClickListener
     lateinit var itemLongClickListener: ItemLongClickListener
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return when(viewType) {
+            HEADER -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.list_photo_header, parent, false)
+                ViewHolder(view)
+            }
+            // TODO: 2022/01/24 item content 타입에 따른 분기 추가
+            CHILD -> {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.list_photo_item_clothes, parent, false)
+                ViewHolder(view)
+            }
+            else -> throw Exception("정의되지 않은 타입")
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data[position]
+        when(item.type) {
+            HEADER -> {
+                holder.bindHeader(item)
+            }
+            CHILD -> {
+                holder.bindChild(item)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
+        return data.size
     }
 
     interface ItemClickListener {
@@ -24,8 +61,21 @@ class ExpandableRecyclerViewAdapter: RecyclerView.Adapter<ViewHolder>() {
     interface ItemLongClickListener {
         fun onLongClick(position: Int)
     }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bindHeader(item: PhotoItem<T>) {
+            itemView.findViewById<TextView>(R.id.textView_photo_header).text = item.text
+        }
+
+        // TODO: 2022/01/24 item content 타입에 따른 분기 추가
+        fun bindChild(item: PhotoItem<T>) {
+            when(item.content) {
+                item.content is Clothes -> {
+                    val imageView = itemView.findViewById<ImageView>(R.id.imageView_photoItem_clothes)
+                    Glide.with(context).load((item.content as Clothes).url).centerCrop().into(imageView)
+                }
+            }
+        }
+    }
 }
 
-class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
-}
