7f3410f401ca55c031802448ea3db87da7e952da
김민수 <ms001118@naver.com>
2022-01-24 00:00:23 +0900

    MO-5 PhotoItem content 제너릭 필드 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/dto/PhotoItem.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/dto/PhotoItem.kt
@@ -1,6 +1,6 @@
 package com.strait.ivblanc.data.model.dto
 
-class PhotoItem<T>(val type: Int, var text:String? = null) {
+class PhotoItem<T>(val type: Int, var text:String? = null, var content: T) {
     private val invisibleItems = arrayListOf<T>()
     fun addInvisibleItem(invisibleItem: T) = invisibleItems.add(invisibleItem)
     fun clearInvisibleItem() = invisibleItems.clear()
