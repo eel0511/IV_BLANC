fae6359aa9e4dc2572d8a82f64891642cfc00560
김민수 <ms001118@naver.com>
2022-01-23 18:04:50 +0900

    MO-5 expandable recyclerview adapter 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/ExpandableRecyclerViewAdapter.kt
@@ -0,0 +1,20 @@
+package com.strait.ivblanc.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ExpandableRecyclerViewAdapter: RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+    }
+}
+
+class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+}
