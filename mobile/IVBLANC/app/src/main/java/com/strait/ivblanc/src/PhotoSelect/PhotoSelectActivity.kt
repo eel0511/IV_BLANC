18738b489abc510ac30b1163b24d9be8bddf488b
김민수 <ms001118@naver.com>
2022-01-24 11:41:20 +0900

    MO-45 사진 선택 액티비티 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/PhotoSelect/PhotoSelectActivity.kt
@@ -0,0 +1,14 @@
+package com.strait.ivblanc.src.PhotoSelect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.strait.ivblanc.R
+import com.strait.ivblanc.config.BaseActivity
+import com.strait.ivblanc.databinding.ActivityPhotoSelectBinding
+
+class PhotoSelectActivity : BaseActivity<ActivityPhotoSelectBinding>(ActivityPhotoSelectBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
