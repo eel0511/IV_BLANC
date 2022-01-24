eded2a7e989aff3cc17fb17f817aad364bcc8a6e
김민수 <ms001118@naver.com>
2022-01-24 20:06:11 +0900

    MO-274 권한 설명 다이얼로그 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/ui/PermissionDialog.kt
@@ -0,0 +1,68 @@
+package com.strait.ivblanc.ui
+
+import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import com.strait.ivblanc.R
+
+class PermissionDialog(val context: Context) {
+
+    lateinit var negativeText: String
+    lateinit var positiveText: String
+    lateinit var content: String
+    lateinit var positiveClickListener: View.OnClickListener
+    lateinit var negativeClickListener: View.OnClickListener
+
+    private val dialog = Dialog(context).apply{
+        setContentView(R.layout.dialog_permission)
+        setCanceledOnTouchOutside(true)
+        setCancelable(true)
+    }
+
+    fun setContent(content: String): PermissionDialog {
+        this.content = content
+        return this
+    }
+
+    fun setNegativeButtonText(text: String): PermissionDialog {
+        this.negativeText = text
+        return this
+    }
+
+    fun setPositiveButtonText(text: String): PermissionDialog {
+        this.positiveText = text
+        return this
+    }
+
+    fun setOnPositiveClickListener(clickListener: View.OnClickListener): PermissionDialog {
+        positiveClickListener = clickListener
+        return this
+    }
+
+    fun setOnNegativeClickListener(clickListener: View.OnClickListener): PermissionDialog {
+        negativeClickListener = clickListener
+        return this
+    }
+
+    fun build(): Dialog {
+        if(this::content.isInitialized) {
+            dialog.findViewById<TextView>(R.id.textView_permissionD_content).text = this.content
+        }
+        if(this::negativeText.isInitialized) {
+            dialog.findViewById<TextView>(R.id.textView_permissionD_negative).text = this.negativeText
+        }
+        if(this::positiveText.isInitialized) {
+            dialog.findViewById<TextView>(R.id.textView_permissionD_positive).text = this.positiveText
+        }
+        if(this::positiveClickListener.isInitialized) {
+            dialog.findViewById<TextView>(R.id.textView_permissionD_positive).setOnClickListener(positiveClickListener)
+        }
+        if(this::negativeClickListener.isInitialized) {
+            dialog.findViewById<TextView>(R.id.textView_permissionD_negative).setOnClickListener(negativeClickListener)
+        }
+        return dialog
+    }
+
+
+}
