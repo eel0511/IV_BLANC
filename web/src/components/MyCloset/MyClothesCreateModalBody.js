06d30965382470d272bf73a7bc5b6c2175345fc1
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-01-24 17:15:07 +0900

    FE-328 옷 등록 색상 라디오버튼 생성

--- web/src/components/MyCloset/MyClothesCreateModalBody.js
+++ web/src/components/MyCloset/MyClothesCreateModalBody.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function MyClothesCreateModalBody() {
+  const colorList = [
+    '빨강',
+    '주황',
+    '베이지',
+    '노랑',
+    '핑크',
+    '초록',
+    '하늘',
+    '파랑',
+    '보라',
+    '갈색',
+    '회색',
+    '네이비',
+    '검정',
+  ];
+  const [selectColor, setSelectColor] = useState('빨강');
+
+  const handleChange = (e) => {
+    console.log('hello');
+  };
+
   return (
-    <div className='RadioForm'>
-      <div className='form-check'>
-        <input
-          className='form-check-input'
-          type='radio'
-          name='flexRadioDefault'
-          id='flexRadioDefault1'
-        />
-        <label className='form-check-label' for='flexRadioDefault1'>
-          빨강
-        </label>
-      </div>
-      <div className='form-check'>
-        <input
-          className='form-check-input'
-          type='radio'
-          name='flexRadioDefault'
-          id='flexRadioDefault2'
-        />
-        <label className='form-check-label' for='flexRadioDefault2'>
-          주황
-        </label>
-      </div>
-    </div>
+    <form className='RadioForm'>
+      <div><h2>색상</h2></div>
+      {colorList.map((color, index) => {
+        return (
+          <React.Fragment key={index}>
+            <input
+              id={color}
+              value={color}
+              className='form-check-input'
+              type='radio'
+              name='colorGroup'
+              onChange={handleChange()}
+            />
+            <label className='form-check-label' htmlFor={color}>
+              {color}
+            </label>
+          </React.Fragment>
+        );
+      })}
+    </form>
   );
 }
