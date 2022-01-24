6079ebd394ca3521300c67403e0d850301b7a9d9
김민수 <ms001118@naver.com>
2022-01-24 21:47:53 +0900

    MO-274 패키지 수정

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/PhotoSelectActivity.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/PhotoSelectActivity.kt
@@ -1,14 +1,28 @@
-package com.strait.ivblanc.src.PhotoSelect
+package com.strait.ivblanc.src.photoSelect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.strait.ivblanc.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.strait.ivblanc.config.BaseActivity
 import com.strait.ivblanc.databinding.ActivityPhotoSelectBinding
 
 class PhotoSelectActivity : BaseActivity<ActivityPhotoSelectBinding>(ActivityPhotoSelectBinding::inflate) {
+    lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewPager = binding.viewpagerPhotoSelectA
 
+        val viewPagerAdapter = ScreenSlidePagerAdapter(this)
+
+        viewPager.adapter = viewPagerAdapter
+    }
+
+    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = 2
+
+        override fun createFragment(position: Int): Fragment = AlbumFragment()
     }
 }
