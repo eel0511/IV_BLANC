42566e4bff1267d891ff54b441a2a3e702dc3e50
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-01-26 12:46:05 +0900

    FE-328 색상,소재,카테고리 UI 구조 구현

--- web/src/components/MyCloset/MyClothesCreateModalBody.js
+++ web/src/components/MyCloset/MyClothesCreateModalBody.js
@@ -2,28 +2,29 @@ import React, { useState } from 'react';
 import codeData from '../../codeData.json';
 
 export default function MyClothesCreateModalBody() {
-  const colorList = [
-    '빨강',
-    '주황',
-    '베이지',
-    '노랑',
-    '핑크',
-    '초록',
-    '하늘',
-    '파랑',
-    '보라',
-    '갈색',
-    '회색',
-    '네이비',
-    '검정',
-  ];
   const [selectColor, setSelectColor] = useState('빨강');
+  const [selectMaterial, setSelectMaterial] = useState('면');
+  const [selectMainCategory, setSelectMainCategory] = useState('상의');
+  const [selectSubCategory, setSelectSubCategory] = useState();
 
-  const handleChange = (e) => {
-    // setSelectColor(e.target.value);
-    Object.entries(codeData['colors']).map((a) => {
-      console.log(a);
-    });
+  const colorHandleChange = (e) => {
+    setSelectColor(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  const materialHandleChange = (e) => {
+    setSelectMaterial(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  const mainCategoryHandleChange = (e) => {
+    setSelectMainCategory(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  const subCategoryHandleChange = (e) => {
+    setSelectSubCategory(e.target.value);
+    // console.log(e.target.value);
   };
 
   return (
@@ -31,24 +32,97 @@ export default function MyClothesCreateModalBody() {
       <div>
         <h2>색상</h2>
       </div>
-      {colorList.map((color, index) => {
+      {Object.entries(codeData['colors']).map(colorArray => {
         return (
-          <React.Fragment key={index}>
+          <React.Fragment key={colorArray[1]}>
             <input
-              id={color}
-              value={color}
+              id={colorArray[0]}
+              value={colorArray[0]}
               className='form-check-input'
               type='radio'
               name='colorGroup'
-              defaultChecked={selectColor === color ? true : false}
-              onChange={handleChange}
+              defaultChecked={selectColor === colorArray[0] ? true : false}
+              onChange={colorHandleChange}
             />
-            <label className='form-check-label' htmlFor={color}>
-              {color}
+            <label className='form-check-label' htmlFor={colorArray[0]}>
+              {colorArray[0]}
             </label>
           </React.Fragment>
         );
       })}
+      <hr/>
+      <div>
+        <h2>소재</h2>
+      </div>
+      {Object.entries(codeData['material']).map(materialArray => {
+        return (
+          <React.Fragment key={materialArray[1]}>
+            <input
+              id={materialArray[0]}
+              value={materialArray[0]}
+              className='form-check-input'
+              type='radio'
+              name='materialGroup'
+              defaultChecked={selectMaterial === materialArray[0] ? true : false}
+              onChange={materialHandleChange}
+            />
+            <label className='form-check-label' htmlFor={materialArray[0]}>
+              {materialArray[0]}
+            </label>
+          </React.Fragment>
+        );
+      })}
+      <hr/>
+      <div>
+        <h2>카테고리</h2>
+      </div>
+      {Object.entries(codeData['category']).map((mainCategoryArray, index) => {
+        return (
+          <React.Fragment key={index}>
+            <input
+              id={mainCategoryArray[0]}
+              value={mainCategoryArray[0]}
+              className='form-check-input'
+              type='radio'
+              name='mainCategoryGroup'
+              defaultChecked={selectMainCategory === mainCategoryArray[0] ? true : false}
+              onChange={mainCategoryHandleChange}
+            />
+            <label className='form-check-label' htmlFor={mainCategoryArray[0]}>
+              {mainCategoryArray[0]}
+            </label>
+          </React.Fragment>
+        );
+      })}
+      <div>
+        {Object.entries(codeData['category']).map((mainCategoryArray, index) => {
+          return (
+            <React.Fragment key={index}>
+              {
+                selectMainCategory === mainCategoryArray[0] && (
+                  Object.entries(mainCategoryArray[1]).map(subCategoryArray => {
+                    return (
+                      <React.Fragment key={subCategoryArray[1]}>
+                        <input
+                          id={subCategoryArray[0]}
+                          value={subCategoryArray[0]}
+                          className='form-check-input'
+                          type='radio'
+                          name='subCategoryGroup'
+                          onChange={subCategoryHandleChange}
+                        />
+                        <label className='form-check-label' htmlFor={subCategoryArray[0]}>
+                          {subCategoryArray[0]}
+                        </label>
+                      </React.Fragment>
+                    );
+                  })
+                )
+              }
+            </React.Fragment>
+          );
+        })}
+      </div>
     </form>
   );
 }
