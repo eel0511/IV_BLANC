0d231c790994aea113a9b57609cf054ad79e6136
김민수 <ms001118@naver.com>
2022-01-26 14:52:41 +0900

    MO-268 ClothesResponse data 필드 오버라이드

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/response/ClothesResponse.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/response/ClothesResponse.kt
@@ -1,7 +1,8 @@
 package com.strait.ivblanc.data.model.response
 
 import com.strait.ivblanc.config.BaseResponse
+import com.strait.ivblanc.data.model.dto.Clothes
 
 class ClothesResponse: BaseResponse() {
-
+    override val data: List<Clothes>? = super.data as? List<Clothes>
 }
