1000ea1aa246b8107c252f556b381d2cef23f90b
SuhyeongLee <eel0511@gmail.com>
2022-01-26 10:03:33 +0900

    BE-240 Friend parameter 검증 추가

--- server/api-module/src/main/java/com/ivblanc/api/controller/FriendController.java
+++ server/api-module/src/main/java/com/ivblanc/api/controller/FriendController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,7 @@ public class FriendController {
 	@ApiOperation(value = "전체친구조회(수락여부 상관없이)", notes = "수락여부 상관없이 자신이 신청한 모든 목록 return")
 	@GetMapping(value = "/all")
 	public ListResult<FriendResDTO>
-	findAllFriend(String applicant) throws Exception {
+	findAllFriend(@RequestParam String applicant) throws Exception {
 		return responseService.getListResult(
 			friendService.MakeFriendToResDTO(friendService.findUserFriends(applicant)));
 	}
@@ -45,7 +46,7 @@ public class FriendController {
 		+ "친구옷장가기 등에서 이것을 이용해 수락된 친구 리스트를 뽑아낼 수 있을거같습니다.")
 	@GetMapping(value = "/isaccept")
 	public ListResult<FriendResDTO>
-	findAllAcceptFriend(String applicant) throws Exception {
+	findAllAcceptFriend(@RequestParam String applicant) throws Exception {
 		return responseService.getListResult(
 			friendService.MakeFriendToResDTO(friendService.findUserFriendsIsAccept(applicant)));
 	}
@@ -54,7 +55,7 @@ public class FriendController {
 		+ "이 리스트를 이용해 fcm으로 독촉을 보내던지 아니면 친구요청 취소를 할수있는 페이지에 활용할수있을거같습니다")
 	@GetMapping(value = "/isnotaccept")
 	public ListResult<FriendResDTO>
-	findAllNotAcceptFriend(String applicant) throws Exception {
+	findAllNotAcceptFriend(@RequestParam String applicant) throws Exception {
 		return responseService.getListResult(
 			friendService.MakeFriendToResDTO(friendService.findUserFriendsIsNotAccept(applicant)));
 	}
@@ -64,7 +65,7 @@ public class FriendController {
 		+ "요청한 입장에서는 수락창이 뜨지않게 해야합니다. 요청하고 혼자 수락할수없게")
 	@PostMapping(value = "/isaccept")
 	public @ResponseBody
-	SingleResult<FriendResDTO> acceptFriend(@RequestBody MakeFriendReqDTO req) throws Exception {
+	SingleResult<FriendResDTO> acceptFriend(@Valid @RequestBody MakeFriendReqDTO req) throws Exception {
 		friendService.makeFriend(friendService.findUserFreind(req.getApplicant(), req.getFriendName()));
 		return responseService.getSingleResult(new FriendResDTO(req.getFriendName()));
 
@@ -73,7 +74,7 @@ public class FriendController {
 	@ApiOperation(value = "친구삭제", notes = "친구가 수락된 상태에서 양방향 연결 둘다 삭제합니다.")
 	@DeleteMapping(value = "/delete")
 	public @ResponseBody
-	SingleResult<FriendResDTO> deleteFriend(@Valid MakeFriendReqDTO req) throws Exception {
+	SingleResult<FriendResDTO> deleteFriend(@Valid @RequestBody MakeFriendReqDTO req) throws Exception {
 
 		friendService.deleteFriend(friendService.findUserFreind(req.getApplicant(), req.getFriendName()));
 		friendService.deleteFriend(friendService.findUserFreind(req.getFriendName(), req.getApplicant()));
@@ -84,7 +85,7 @@ public class FriendController {
 		+ "단방향으로 연결된 상태이므로 요청자의 입장에서만 사용가능하게 해야합니다")
 	@DeleteMapping(value = "/cancel")
 	@ResponseBody
-	SingleResult<FriendResDTO> cancelFriend(@Valid MakeFriendReqDTO req) throws Exception {
+	SingleResult<FriendResDTO> cancelFriend(@Valid @RequestBody MakeFriendReqDTO req) throws Exception {
 		friendService.deleteFriend(friendService.findUserFreind(req.getApplicant(), req.getFriendName()));
 		return responseService.getSingleResult(new FriendResDTO(req.getFriendName()));
 	}
@@ -93,7 +94,7 @@ public class FriendController {
 		+ "이때 api 요청과함께 fcm등을 보낼수 있을거같습니다.")
 	@PostMapping(value = "/request")
 	public @ResponseBody
-	SingleResult<FriendResDTO> addFriend(@RequestBody MakeFriendReqDTO req) throws Exception {
+	SingleResult<FriendResDTO> addFriend(@Valid @RequestBody MakeFriendReqDTO req) throws Exception {
 		friendService.addFriend(req);
 		System.out.println(req.getFriendName());
 		return responseService.getSingleResult(new FriendResDTO(req.getFriendName()));
@@ -103,7 +104,7 @@ public class FriendController {
 		+ "여기서 수락버튼을 만들어 수락시 아래의 친구수락 api로 쏘면될거같습니다")
 	@GetMapping(value = "/friendrequest")
 	public ListResult<FriendResDTO>
-	findRequest(String applicant) throws Exception {
+	findRequest(@RequestParam String applicant) throws Exception {
 		return responseService.getListResult(friendService.MakeFriendToResDTO(friendService.findRequest(applicant)));
 	}
 }
