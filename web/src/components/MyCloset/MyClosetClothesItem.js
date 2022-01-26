fdd2aff08e09aad0ba174cffd2bac5f963e885f4
이인섭 <inseob135@gmail.com>
2022-01-26 11:33:46 +0900

    FE-387 옷 상세 페이지 기본 폼 생성

--- web/src/components/MyCloset/MyClosetClothesItem.js
+++ web/src/components/MyCloset/MyClosetClothesItem.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, ListGroup } from 'react-bootstrap';
 import './MyCloset.css';
 
 export default function MyClosetClothesItem({ clothesData }) {
@@ -11,20 +11,28 @@ export default function MyClosetClothesItem({ clothesData }) {
   return (
     <div className='card h-100'>
       <div className='card-body'>
-        <img
-          src={require(`../../assets/${clothesData.url}`)}
-          alt={clothesData.clothesId}
-          style={{ maxWidth: '100%', maxHeight: '100%' }}
-          onClick={handleShow}
-        />
+        <img src={require(`../../assets/${clothesData.url}`)} alt={clothesData.clothesId} style={{ maxWidth: '100%', maxHeight: '100%' }} onClick={handleShow} />
 
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>옷 상세정보 제목</Modal.Title>
+            <Modal.Title>옷 상세정보</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            옷 상세정보 보여주기
-            <p>{clothesData.clothesId}</p>
+            <img src={require(`../../assets/${clothesData.url}`)} alt={clothesData.clothesId} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+            <ListGroup variant='flush' mx>
+              <ListGroup.Item>종류 : {clothesData.category}</ListGroup.Item>
+              <ListGroup.Item>색깔 : {clothesData.color}</ListGroup.Item>
+              <ListGroup.Item>사이즈 : {clothesData.size}</ListGroup.Item>
+              <ListGroup.Item>소재 : {clothesData.material}</ListGroup.Item>
+              <ListGroup.Item>계절 : {clothesData.season}</ListGroup.Item>
+              <ListGroup.Item>싫어요 : {clothesData.dislikePoint}</ListGroup.Item>
+              <ListGroup.Item>등록날짜 : {clothesData.createDate}</ListGroup.Item>
+            </ListGroup>
+            {/* <p>{clothesData.clothesId}</p>
+            <p>{clothesData.category}</p>
+            <p>{clothesData.color}</p>
+            <p>{clothesData.createDate}</p>
+            <p>{clothesData.material}</p> */}
           </Modal.Body>
           <Modal.Footer>
             <Button variant='secondary' onClick={handleClose}>
