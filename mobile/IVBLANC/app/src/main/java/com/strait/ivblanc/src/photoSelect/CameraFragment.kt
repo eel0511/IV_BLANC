3365bfb1eaa27ab238a69a48ee7d47e1d3fc43da
김민수 <ms001118@naver.com>
2022-01-28 06:53:43 +0900

    MO-278 CameraFragment 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/CameraFragment.kt
@@ -0,0 +1,18 @@
+package com.strait.ivblanc.src.photoSelect
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.strait.ivblanc.R
+import com.strait.ivblanc.config.BaseFragment
+import com.strait.ivblanc.databinding.FragmentCameraBinding
+
+class CameraFragment : BaseFragment<FragmentCameraBinding>(FragmentCameraBinding::bind, R.layout.fragment_camera) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
