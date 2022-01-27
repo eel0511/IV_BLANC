b54012a976ebe9a45456a0f23eda0fc088b63fcc
김민수 <ms001118@naver.com>
2022-01-28 01:12:30 +0900

    MO-276 PhotoRecyclerViewAdapter 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/PhotoRecyclerViewAdapter.kt
@@ -0,0 +1,33 @@
+package com.strait.ivblanc.adapter
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.strait.ivblanc.R
+
+class PhotoRecyclerViewAdapter(private var data: List<Uri>): RecyclerView.Adapter<PhotoRecyclerViewAdapter.ViewHolder>() {
+    lateinit var itemClickListener: ItemClickListener
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bind(item: Uri) {
+            itemView.findViewById<ImageView>(R.id.imageView_photoItem_clothes).setImageURI(item)
+            itemView.setOnClickListener{
+                itemClickListener.onClick(adapterPosition)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_photo_item_clothes, parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(data[position])
+
+    override fun getItemCount(): Int = data.size
+
+    interface ItemClickListener {
+        fun onClick(position: Int)
+    }
+}
