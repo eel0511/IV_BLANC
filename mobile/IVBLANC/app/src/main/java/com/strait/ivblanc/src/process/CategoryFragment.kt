04d29fb23d55e97dbabcb5dd4fd53074e5bca17d
김민수 <ms001118@naver.com>
2022-01-29 03:24:56 +0900

    MO-282 CategoryFragment 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/process/CategoryFragment.kt
@@ -0,0 +1,18 @@
+package com.strait.ivblanc.src.process
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.strait.ivblanc.R
+import com.strait.ivblanc.config.BaseFragment
+import com.strait.ivblanc.databinding.FragmentCategoryBinding
+
+class CategoryFragment : BaseFragment<FragmentCategoryBinding>(FragmentCategoryBinding::bind, R.layout.fragment_category) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
