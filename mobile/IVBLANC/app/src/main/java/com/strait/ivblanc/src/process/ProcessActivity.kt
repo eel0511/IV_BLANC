072203cd46ace826cf449b6492e5e972d059335d
김민수 <ms001118@naver.com>
2022-01-29 01:54:41 +0900

    MO-143 이미지 전처리 액티비티 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/process/ProcessActivity.kt
@@ -0,0 +1,11 @@
+package com.strait.ivblanc.src.process
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProcessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_process)
+    }
+}
