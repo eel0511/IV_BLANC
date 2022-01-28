b6ebc1260786215940a66f4db605171e91101736
김민수 <ms001118@naver.com>
2022-01-29 05:22:21 +0900

    MO-282 SeasonFragment 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/process/SeasonFragment.kt
@@ -0,0 +1,13 @@
+package com.strait.ivblanc.src.process
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.strait.ivblanc.R
+import com.strait.ivblanc.config.BaseFragment
+import com.strait.ivblanc.databinding.FragmentSeasonBinding
+
+class SeasonFragment : BaseFragment<FragmentSeasonBinding>(FragmentSeasonBinding::bind, R.layout.fragment_season) {
+}
