2bab732694949c2c269bcc8355c0b73a33553f3d
김민수 <ms001118@naver.com>
2022-01-28 11:49:34 +0900

    MO-278 사진찍기 함수 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/CameraFragment.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/CameraFragment.kt
@@ -1,20 +1,26 @@
 package com.strait.ivblanc.src.photoSelect
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
@@ -23,6 +29,8 @@ import com.strait.ivblanc.config.BaseFragment
 import com.strait.ivblanc.databinding.FragmentCameraBinding
 import com.strait.ivblanc.ui.PermissionDialog
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 private const val TAG = "CameraFragment_해협"
 class CameraFragment : BaseFragment<FragmentCameraBinding>(FragmentCameraBinding::bind, R.layout.fragment_camera) {
@@ -125,6 +133,39 @@ class CameraFragment : BaseFragment<FragmentCameraBinding>(FragmentCameraBinding
                 Log.d(TAG, "Use case binding failed: ", e)
             }
         }, ContextCompat.getMainExecutor(requireActivity()))
+    }
 
+    private var imageCapture: ImageCapture? = null
+
+    private fun takePicture() {
+        val imageCapture = this.imageCapture?: return
+
+        //MediaStore에 저장할 파일 이름 생성
+        val name = SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.KOREA).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/IVBLANC")
+            }
+        }
+
+        // 파일과 메타데이터를 포함하는 아웃풋 옵션 설정
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(requireActivity().contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            .build()
+
+        //캡처 리스너 세팅, 이벤트 발생하면 위에서 지정한 경로로 이미지 저장
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(requireActivity()), object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    toast("이미지가 저장되었습니다.", Toast.LENGTH_SHORT)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.d(TAG, "onCaptureSavedError: ${exception.message}")
+                }
+            }
+        )
     }
 }
