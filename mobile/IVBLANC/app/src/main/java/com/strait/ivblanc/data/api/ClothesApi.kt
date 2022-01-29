af3cfa2bf032301f6c17f14f502c5c7c96e6cf44
김민수 <ms001118@naver.com>
2022-01-29 23:56:02 +0900

    MO-356 access token 사용하는 rest api로 변경

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/ClothesApi.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/ClothesApi.kt
@@ -9,6 +9,6 @@ interface ClothesApi {
     // TODO: 2022/01/26 access token 사용시 변경
     @GET("/api/clothes/all")
 
-//    suspend fun getAllClothes(@Query("page") page: Int): Response<ClothesResponse>
-    suspend fun getAllClothes(@Query("page") page: Int, @Query("userId")userId: Int): Response<ClothesResponse>
+    suspend fun getAllClothes(@Query("page") page: Int): Response<ClothesResponse>
+//    suspend fun getAllClothes(@Query("page") page: Int, @Query("userId")userId: Int): Response<ClothesResponse>
 }
