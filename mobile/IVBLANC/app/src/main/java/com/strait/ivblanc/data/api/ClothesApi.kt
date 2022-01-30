44948aeaf738c3b5450b9a74b6c2ad5211434320
김민수 <ms001118@naver.com>
2022-01-30 10:58:46 +0900

    MO-32 ClothesApi 카테고리 별 조회 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/ClothesApi.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/ClothesApi.kt
@@ -8,7 +8,9 @@ import retrofit2.http.Query
 interface ClothesApi {
     // TODO: 2022/01/26 access token 사용시 변경
     @GET("/api/clothes/all")
-
     suspend fun getAllClothes(@Query("page") page: Int): Response<ClothesResponse>
-//    suspend fun getAllClothes(@Query("page") page: Int, @Query("userId")userId: Int): Response<ClothesResponse>
+
+    @GET("/api/clothes/category")
+    suspend fun getClothesByCategory(@Query("category") category: Int, @Query("page") page: Int): Response<ClothesResponse>
+
 }
