f7fea504f96f43a2ca7264bdf1cc5a2ad26bb60e
김민수 <ms001118@naver.com>
2022-01-30 12:23:49 +0900

    MO-32 카테고리 코드 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/util/CategoryCode.kt
@@ -0,0 +1,43 @@
+package com.strait.ivblanc.util
+
+class CategoryCode {
+    companion object {
+        val TOTAL = 0
+        val T_SHIRT = 10
+        val SHIRT = 11
+        val HOOD = 12
+        val SWEAT_SHIRT = 13
+        val BLOUSE = 14
+        val CROP_TOP = 15
+        val DRESS = 16
+        val TOP_ETC = 17
+        val DENIM_PANTS = 20
+        val COTTON_PANTS = 21
+        val SHORT_PANTS = 22
+        val SUIT_PANTS = 23
+        val SPORT_PANTS = 24
+        val SKIRT = 25
+        val LEGGINGS = 26
+        val PANTS_ETC = 27
+        val COAT = 40
+        val JACKET = 41
+        val PADDING = 42
+        val VEST = 43
+        val CARDIGAN = 44
+        val OUTER_ETC = 45
+        val SNEAKERS = 50
+        val SPORTS_SHOES = 51
+        val LOAFERS = 52
+        val BOOTS = 53
+        val SLIPPERS = 54
+        val HEELS = 55
+        val SHOES_ETC = 56
+        val BACKPACK = 60
+        val CROSS_BAG = 61
+        val SHOULDER_BAG = 62
+        val HANDBAG = 63
+        val BAG_ETC = 63
+        val HAT = 70
+    }
+
+}
