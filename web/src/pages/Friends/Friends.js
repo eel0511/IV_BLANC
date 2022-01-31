1363edf830ae2464ff9a1718976a4364bb8ba335
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-01-31 13:27:36 +0900

    FE-383 친구옷장 파일구조 생성

--- web/src/pages/Friends/Friends.js
+++ web/src/pages/Friends/Friends.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import FriendsList from '../../layouts/Friends/FriendsList';
+import FriendsCreateButton from '../../components/Friends/FriendsCreateButton';
 
 export default function Friends() {
-  return <h1>Friends</h1>;
+  return (
+    <>
+      <h1>Friends</h1>
+      <body><FriendsList /></body>
+      <footer><FriendsCreateButton /></footer>
+    </>
+  );
 }
