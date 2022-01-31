b41059072d6c99b37b3120e180f6349d0bf99124
이인섭 <inseob135@gmail.com>
2022-01-31 13:01:02 +0900

    FE-365 탭, 아바타 컨테이너 나누기

--- web/src/pages/MyStyle/MyStyle.js
+++ web/src/pages/MyStyle/MyStyle.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import MyClosetSidebar from '../../layouts/MyCloset/MyClosetSidebar';
 
 export default function MyStyle() {
-  return <h1>Mystyle</h1>;
+  return (
+    <>
+      <h1>Mystyle</h1>
+      <div className='MyStyleContanier'>
+        <Container fluid='md'>
+          <Row>
+            <Col sm={4}>1of2</Col>
+            <Col sm={8}>2of2</Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
