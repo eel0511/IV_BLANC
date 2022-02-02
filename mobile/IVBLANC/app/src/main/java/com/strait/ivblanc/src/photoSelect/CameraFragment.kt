ce8cd4ef05a150c79b5d2b5909656cd243c67920
김민수 <ms001118@naver.com>
2022-02-02 13:20:27 +0900

    MO-278 카메라 권한 요청 Activity로 이동

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/CameraFragment.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/CameraFragment.kt
@@ -34,80 +34,13 @@ import java.util.*
 
 private const val TAG = "CameraFragment_해협"
 class CameraFragment : BaseFragment<FragmentCameraBinding>(FragmentCameraBinding::bind, R.layout.fragment_camera) {
-    lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
-    private var isGranted = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                this.isGranted = true
-            } else {
-                showReasonForPermission()
-            }
-        }
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        checkReadStoragePermission()
         binding.buttonCameraFShot.setOnClickListener {
             takePicture()
         }
     }
-
-    //권한 체크가 되어있지 않다면 권한 체크 실행
-    override fun onResume() {
-        if(isGranted) startCamera()
-        else checkReadStoragePermission()
-        super.onResume()
-    }
-
-    // 권한관련 함수 시작
-    private fun checkReadStoragePermission() {
-        when {
-            // 권한이 있을 때 카메라 실행
-            ContextCompat.checkSelfPermission(
-                requireActivity(),
-                Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                this.isGranted = true
-                return
-            }
-            //사용자가 명시적으로 권한을 거부했을 때 -> true
-            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
-                showReasonForPermission()
-            }
-            //사용자가 권한요청을 처음 보거나, 다시 묻지 않음, 권한요청을 허용한 경우 -> false
-            // requestPermissionLauncher는 수 회 이상 권한을 거부했을 경우 launch 되지 않음
-            else -> {
-                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-            }
-        }
-    }
-
-    // 권한이 필요한 이유를 설명하는 다이얼로그 제공
-    // positive button에 권한 설정으로 이동하는 클릭리스너 세팅
-    private fun showReasonForPermission() {
-        PermissionDialog(requireActivity())
-            .setContent("사진을 찍기 위해 필요한 권한입니다.")
-            .setPositiveButtonText("권한 설정하기")
-            .setOnPositiveClickListener(object : View.OnClickListener {
-                override fun onClick(p0: View?) {
-                    val intent = Intent(
-                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                        Uri.parse("package:${requireActivity().packageName}")
-                    ).apply {
-                        this.addCategory(Intent.CATEGORY_DEFAULT)
-                        this.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    }
-                    requireActivity().startActivity(intent)
-                }
-            }).build().show()
-    }
-    //권한 관련 함수 끝
 
     //카메라 관련 함수 시작
     private var imageCapture: ImageCapture? = null
