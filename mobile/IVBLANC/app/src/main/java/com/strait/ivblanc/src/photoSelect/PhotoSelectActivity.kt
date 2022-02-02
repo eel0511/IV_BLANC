f00d4faf6e1af0119ae6259032ef9dbf523db4f3
김민수 <ms001118@naver.com>
2022-02-02 13:04:03 +0900

    MO-348 권한 체크 액티비티로 이동

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/PhotoSelectActivity.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/PhotoSelectActivity.kt
@@ -1,29 +1,46 @@
 package com.strait.ivblanc.src.photoSelect
 
-import android.os.Build
+import android.Manifest
 import android.os.Bundle
-import android.util.Log
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.strait.ivblanc.config.BaseActivity
 import com.strait.ivblanc.databinding.ActivityPhotoSelectBinding
+import com.strait.ivblanc.util.PermissionUtil
 
 private const val TAG = "PhotoSelectActivity_해협"
 class PhotoSelectActivity : BaseActivity<ActivityPhotoSelectBinding>(ActivityPhotoSelectBinding::inflate) {
     lateinit var viewPager: ViewPager2
+    lateinit var permissionUtil: PermissionUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        init()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkPermissions()
+    }
+
+    private fun init() {
+        permissionUtil = PermissionUtil(this)
+    }
+
+    private fun initView() {
         viewPager = binding.viewpagerPhotoSelectA
-
         val viewPagerAdapter = ScreenSlidePagerAdapter(this)
-
         viewPager.adapter = viewPagerAdapter
+    }
+
+    private fun checkPermissions() {
+        if(!permissionUtil.checkPermissions(listOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA))) {
+            permissionUtil.requestPermissions()
+        } else {
+            initView()
+        }
     }
 
     private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
