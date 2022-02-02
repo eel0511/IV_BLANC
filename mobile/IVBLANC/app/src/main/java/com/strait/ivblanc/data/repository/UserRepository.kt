5252ef0582469a0ef75131bfadc6f9e0ba7b45a1
김민수 <ms001118@naver.com>
2022-02-03 00:44:19 +0900

    MO-278 경고 메세지 Resource.getSystem() 삭제

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/UserRepository.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/UserRepository.kt
@@ -15,7 +15,6 @@ import java.lang.Exception
 
 class UserRepository {
     var userApi: UserApi = ApplicationClass.sRetrofit.create(UserApi::class.java)
-    private val resource = Resources.getSystem()
 
     suspend fun join(user: UserForJoin): Resource<JoinResponse> {
         return try {
@@ -61,10 +60,10 @@ class UserRepository {
                     Resource.error(null, response.body()!!.message!!)
                 }
             } else {
-                Resource.error(null, resource.getString(R.string.networkErrorMessage))
+                Resource.error(null, "알 수 없는 오류입니다.")
             }
         } catch (e: Exception) {
-            Resource.error(null, resource.getString(R.string.unknownErrorMessage))
+            Resource.error(null, "서버와 연결할 수 없습니다. 잠시 후 다시 시도해 주세요.")
         }
     }
 }
