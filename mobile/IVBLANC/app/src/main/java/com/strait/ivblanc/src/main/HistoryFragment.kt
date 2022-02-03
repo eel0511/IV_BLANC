a1b3ed8b18bf19cbcf8c043ddf9bd2edfa82398e
김민수 <ms001118@naver.com>
2022-02-03 11:28:02 +0900

    MO-379 HistoryFragment 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/main/HistoryFragment.kt
@@ -0,0 +1,13 @@
+package com.strait.ivblanc.src.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.strait.ivblanc.R
+import com.strait.ivblanc.config.BaseFragment
+import com.strait.ivblanc.databinding.FragmentHistoryBinding
+
+class HistoryFragment : BaseFragment<FragmentHistoryBinding>(FragmentHistoryBinding::bind, R.layout.fragment_history) {
+}
