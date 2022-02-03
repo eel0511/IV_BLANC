16f855874b8495b4f0eef668363b326585ce104a
김민수 <ms001118@naver.com>
2022-02-04 02:06:15 +0900

    MO-376 PhotoSelectViewModel의 값을 이용하여 toolbar 뷰 변경

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/PhotoSelectActivity.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/photoSelect/PhotoSelectActivity.kt
@@ -2,11 +2,16 @@ package com.strait.ivblanc.src.photoSelect
 
 import android.Manifest
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.strait.ivblanc.R
 import com.strait.ivblanc.config.BaseActivity
+import com.strait.ivblanc.data.model.viewmodel.PhotoSelectViewModel
 import com.strait.ivblanc.databinding.ActivityPhotoSelectBinding
 import com.strait.ivblanc.util.PermissionUtil
 
@@ -14,7 +19,7 @@ private const val TAG = "PhotoSelectActivity_해협"
 class PhotoSelectActivity : BaseActivity<ActivityPhotoSelectBinding>(ActivityPhotoSelectBinding::inflate) {
     lateinit var viewPager: ViewPager2
     lateinit var permissionUtil: PermissionUtil
-
+    private val photoSelectViewModel: PhotoSelectViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         init()
@@ -35,6 +40,52 @@ class PhotoSelectActivity : BaseActivity<ActivityPhotoSelectBinding>(ActivityPho
     }
 
     private fun initView() {
+        setViewPager()
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(binding.toolbarPhotoSelect.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        photoSelectViewModel.toolbarTitle.observe(this) {
+            setToolbarTitle(it)
+        }
+        photoSelectViewModel.leadingIconDrawable.observe(this) {
+            setLeadingIcon(it, getListener(it))
+        }
+
+        photoSelectViewModel.trailingIconDrawable.observe(this) {
+            setTrailingIcon(it, getListener(it))
+        }
+    }
+
+    // imageView의 background drawable Id에 따라 버튼 리스너 반환
+    private fun getListener(resId: Int): View.OnClickListener {
+        return when(resId) {
+            R.drawable.ic_close -> object: View.OnClickListener {
+                override fun onClick(v: View?) {
+                    this@PhotoSelectActivity.onBackPressed()
+                }
+            }
+            else -> View.OnClickListener { }
+        }
+    }
+
+    private fun setToolbarTitle(title: String) {
+        binding.toolbarPhotoSelect.textViewToolbar.text = title
+    }
+
+    private fun setLeadingIcon(resId: Int, clickListener: View.OnClickListener) {
+        binding.toolbarPhotoSelect.imageViewToolbarLeadingIcon.background = ResourcesCompat.getDrawable(resources, resId, null)
+        binding.toolbarPhotoSelect.imageViewToolbarLeadingIcon.setOnClickListener(clickListener)
+    }
+
+    private fun setTrailingIcon(resId: Int, clickListener: View.OnClickListener) {
+        binding.toolbarPhotoSelect.imageViewToolbarTrailingIcon.background = ResourcesCompat.getDrawable(resources, resId, null)
+        binding.toolbarPhotoSelect.imageViewToolbarTrailingIcon.setOnClickListener(clickListener)
+    }
+
+    private fun setViewPager() {
         viewPager = binding.viewpagerPhotoSelectA
         val viewPagerAdapter = ScreenSlidePagerAdapter(this)
         viewPager.adapter = viewPagerAdapter
