5bc426c14421bf0987e9fe1e4108e4aea9429cf4
김민수 <ms001118@naver.com>
2022-02-03 14:37:42 +0900

    MO-406 ClothesApi 옷 등록 함수 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/ClothesApi.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/ClothesApi.kt
@@ -1,11 +1,11 @@
 package com.strait.ivblanc.data.api
 
+import com.strait.ivblanc.config.BaseResponse
 import com.strait.ivblanc.data.model.response.ClothesDeleteResponse
 import com.strait.ivblanc.data.model.response.ClothesResponse
+import okhttp3.MultipartBody
 import retrofit2.Response
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface ClothesApi {
     @GET("/api/clothes/all")
@@ -13,4 +13,14 @@ interface ClothesApi {
 
     @DELETE("/api/clothes/deleteById")
     suspend fun deleteClothesById(@Query("clothesId") clothesId: Int): Response<ClothesDeleteResponse>
+
+    @Multipart
+        @POST("/api/clothes/add")
+    suspend fun addClothes(@Query("category") category: Int
+                           , @Query("color") color: String
+                           , @Query("material") material: Int
+                           , @Query("season") season: Int
+                           , @Query("size") size: Int
+                           , @Part("file") image: MultipartBody.Part): Response<BaseResponse>
+
 }
