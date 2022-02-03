b96f23645afbb4733f314165f416b6fb4b0c197d
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-02-03 18:00:14 +0900

    FE-393 친구리스트 서버연동 임시저장

--- web/src/layouts/Friends/FriendsList.js
+++ web/src/layouts/Friends/FriendsList.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function FriendsList() {
+  const [friendsList, setFriendsList] = useState([]);
+
+  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJQayI6IjIiLCJpYXQiOjE2NDM4NTQ1MDIsImV4cCI6MTY0NjQ0NjUwMn0.s4B6viyO_tR8lZMUdxW62u82uT08ZltwgEBpuvTBZOQ';
+  const getFriendsList = () => {
+    axios
+      .get('http://i6d104.p.ssafy.io:9999/api/friend/isaccept',
+      { 
+        headers: {
+          "Access-Control-Allow-origin": "*",
+          "Authorization": `Bearer ${token}`
+        },
+        params: {
+          applicant: 'user'
+        }
+      })
+      .then((response) => {
+        setFriendsList(response.data);
+        // console.log(response.data)
+      });
+  };
+
+  // useEffect(() => {
+  //   getFriendsList();
+  // }, []);
+
   return (
     <>
       <h1>Body</h1>
