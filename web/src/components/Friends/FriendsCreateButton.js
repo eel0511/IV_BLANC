8a8133b3e496a993c2446163b9dd0ee32bd53e99
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-02-03 11:55:12 +0900

    FE-385 친구추가 입력 폼 구조 생성

--- web/src/components/Friends/FriendsCreateButton.js
+++ web/src/components/Friends/FriendsCreateButton.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Friends.css';
+import { Modal, Button } from 'react-bootstrap';
 
 export default function FriendsCreateButton() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
-      <h1>footer</h1>
       <img
         className='friedsCreateButton'
         src={require('../../assets/친구등록.png')}
         alt='Create'
         style={{ width: '100px', height: '100px' }}
+        onClick={handleShow}
       />
+      <Modal show={show} onHide={handleClose} centered='true'>
+        <Modal.Header closeButton>
+          <Modal.Title>친구 추가</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form>
+            <label for='friendEmail' className='form-label'>
+              친구 이메일 :
+            </label>
+            <input
+              type='email'
+              className='form-control'
+              id='friendEmail'
+              placeholder='name@example.com'
+            />
+            <Button>요청</Button>
+          </form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
