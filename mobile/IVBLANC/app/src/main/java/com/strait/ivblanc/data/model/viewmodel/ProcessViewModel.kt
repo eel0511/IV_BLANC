8374ec5192693d171ce31b13815e4bc5217d507e
김민수 <ms001118@naver.com>
2022-02-04 21:13:46 +0900

    MO-407 카테고리 셋 분류 함수 추가, 옷 등록에 필요한 정보 LiveData로 저장

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/viewmodel/ProcessViewModel.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/model/viewmodel/ProcessViewModel.kt
@@ -5,10 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.strait.ivblanc.R
 import com.strait.ivblanc.config.ApplicationClass
 import com.strait.ivblanc.data.model.dto.ClothesForUpload
 import com.strait.ivblanc.data.model.response.ClothesDeleteResponse
 import com.strait.ivblanc.data.repository.ClothesRepository
+import com.strait.ivblanc.util.CategoryCode
+import com.strait.ivblanc.util.MaterialCode
 import com.strait.ivblanc.util.Resource
 import com.strait.ivblanc.util.Status
 import kotlinx.coroutines.Dispatchers
@@ -23,11 +26,30 @@ class ProcessViewModel: ViewModel() {
     private val clothesRepository = ClothesRepository()
     private val _clothesResponseStatus = MutableLiveData<Resource<ClothesDeleteResponse>>()
     val clothesResponseStatus: LiveData<Resource<ClothesDeleteResponse>> get() = _clothesResponseStatus
-    var category: Int? = null
-    var season: Int? = null
-    var color: String? = null
-    var material: Int? = null
-    var size: Int? = null
+
+    val categorySet = CategoryCode().codeSet
+    val materialSet = MaterialCode().codeSet
+
+    private val _largeCategory = MutableLiveData<Int>(CategoryCode.TOP) // CategoryCode 0 ~ 7까지
+    private val _smallCategory = MutableLiveData<Int>(CategoryCode.T_SHIRT) // CategoryCode 대분류에서 가져오기
+    private val _season = MutableLiveData<Int>(1) // 1 - 4
+    private val _color = MutableLiveData<String>("#ffffff") // hex code
+    private val _size = MutableLiveData<Int>(0) // 모든 숫자
+    private val _material = MutableLiveData<Int>(MaterialCode.COTTON) // MaterialCode
+
+    val largeCategory: LiveData<Int> get() = _largeCategory
+    val smallCategory: LiveData<Int> get() = _smallCategory
+    val season: LiveData<Int> get() = _season
+    val color: LiveData<String> get() = _color
+    val size: LiveData<Int> get() = _size
+    val material: LiveData<Int> get() = _material
+
+    fun setLargeCategory(largeCategory: Int) = _largeCategory.postValue(largeCategory)
+    fun setSmallCategory(smallCategory: Int) = _smallCategory.postValue(smallCategory)
+    fun setSeason(season: Int) = _smallCategory.postValue(season)
+    fun setColor(color: String) = _color.postValue(color)
+    fun setMaterial(material: Int) = _material.postValue(material)
+    fun setSize(size: Int) = _size.postValue(size)
 
     fun addClothes(clothes: ClothesForUpload, imgUri: Uri) = viewModelScope.launch {
         setLoading()
@@ -48,4 +70,28 @@ class ProcessViewModel: ViewModel() {
     }
 
     private fun setLoading() = _clothesResponseStatus.postValue(Resource.loading(null))
+
+    val largeCategorySet = getLargeCategories()
+
+    // 전체를 제외한 대분류만 나타낸다
+    private fun getLargeCategories(): List<Pair<Int, Int>> {
+        val result = mutableListOf<Pair<Int, Int>>()
+        categorySet.forEach { entry ->
+            if(entry.key in 1..9) result.add(entry.toPair())
+        }
+        return result
+    }
+
+    // 대분류로부터 소분류 카테고리 분류
+    fun getSmallCategoriesByLargeCategory(largeCategory: Int): List<Pair<Int, Int>> {
+        val result = mutableListOf<Pair<Int, Int>>()
+
+        categorySet.forEach { entry ->
+            if(entry.key.toString()[0] == largeCategory.toString()[0]) {
+                result.add(entry.toPair())
+            }
+        }
+
+        return result
+    }
 }
