63e184d97ec7476ba5dbede2b3128f0915240bf9
김민수 <ms001118@naver.com>
2022-02-04 21:20:45 +0900

    MO-407 대분류, 소분류 입력에 따른 카테고리 요청 및 사이즈 입력

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/process/CategoryFragment.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/process/CategoryFragment.kt
@@ -5,14 +5,80 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
 import com.strait.ivblanc.R
 import com.strait.ivblanc.config.BaseFragment
+import com.strait.ivblanc.data.model.viewmodel.ProcessViewModel
 import com.strait.ivblanc.databinding.FragmentCategoryBinding
 
 class CategoryFragment : BaseFragment<FragmentCategoryBinding>(FragmentCategoryBinding::bind, R.layout.fragment_category) {
-
+    private val processViewModel: ProcessViewModel by activityViewModels()
+    lateinit var largeCategories: List<String>
+    var smallCategories = listOf<Pair<Int, Int>>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        init()
     }
+
+    private fun init() {
+        largeCategories = getLargeCategoryString()
+        val largeCategoryAdapter = ArrayAdapter(requireActivity(), R.layout.list_category_item, largeCategories)
+        binding.autoCompleteCategoryFLarge.setAdapter(largeCategoryAdapter)
+
+        // 대분류가 바뀜에 따라 소분류 바꿈
+        processViewModel.largeCategory.observe(requireActivity()) {
+            smallCategories = processViewModel.getSmallCategoriesByLargeCategory(it)
+            val smallCategoryAdapter = ArrayAdapter(requireActivity(), R.layout.list_category_item, getSmallCategoryStringByLargeCategory(it))
+            binding.autoCompleteCategoryFSmall.setAdapter(smallCategoryAdapter)
+        }
+
+        // 대분류 edittext observe -> viewModel에 대분류 카테고리 변경
+        binding.autoCompleteCategoryFLarge.addTextChangedListener {
+            val largeCategory = processViewModel.largeCategorySet.find { pair ->
+                it.toString() == resources.getString(pair.second) && pair.first in 0..9
+            }
+
+            largeCategory?.let {
+                processViewModel.setLargeCategory(it.first)
+            }
+
+        }
+
+        // 소분류 edittext observe -> viewModel 소분류 카테고리 변경
+        binding.autoCompleteCategoryFSmall.addTextChangedListener {
+            val smallCategory = smallCategories.find {
+                 resources.getString(it.second) == it.toString()
+            }
+
+            smallCategory?.let {
+                processViewModel.setSmallCategory(it.first)
+            }
+        }
+
+        // 사이즈 edittext observe -> viewModel 사이즈 변경
+        binding.editTextCategoryFSize.addTextChangedListener {
+            processViewModel.setSize(it.toString().toInt())
+        }
+    }
+
+    private fun getLargeCategoryString(): List<String> {
+        val result = mutableListOf<String>()
+        processViewModel.largeCategorySet.forEach {
+            result.add(resources.getString(it.second))
+        }
+        return result.toList()
+    }
+
+    private fun getSmallCategoryStringByLargeCategory(largeCategory: Int): List<String> {
+        val result = mutableListOf<String>()
+        processViewModel.getSmallCategoriesByLargeCategory(largeCategory).forEach {
+            result.add(resources.getString(it.second))
+        }
+        return result.toList()
+    }
+
+
 }
