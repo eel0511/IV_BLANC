47080f121576d8383ec2ee1f7949e80a82200c61
이인섭 <inseob135@gmail.com>
2022-02-04 17:37:51 +0900

    FE-405 선택한 옷 출력 임시코드

--- web/src/components/MyStyle/SelectedImage.js
+++ web/src/components/MyStyle/SelectedImage.js
@@ -2,18 +2,44 @@ import React, { useState } from 'react';
 import { imageList } from './SelectedImageList';
 
 function SelectedImage() {
-  const [selectedImg, setSelectedImg] = useState(false);
+  // const [inputs, setInputs] = useState({
+  //   category: 0,
+  //   clothesId: 0,
+  //   color: '',
+  //   count: 0,
+  //   createDate: '',
+  //   dislikePoint: 0,
+  //   favorite: 0,
+  //   material: '',
+  //   season: 0,
+  //   size: 0,
+  //   updateDate: '',
+  //   url: '',
+  //   userId: 0,
+  // });
+
+  // const [selectedImg, setSelectedImg] = imageList;
 
   return (
-    <div className='imgContainer'>
-      {ImageList.map((img, index) => (
-        <img
-          key={index}
-          src={img}
-          alt='이미지'
-          onClick={() => setSelectedImg()}
-        />
-      ))}
+    <div className='wrapper'>
+      <div
+        className='imgContainer'
+        style={{ display: 'flex', flexDirection: 'row' }}
+      >
+        {imageList.map((clothesData) => (
+          <img
+            className='MyStyleClothesItemImg'
+            key={clothesData.clothesId}
+            src={require(`../../assets/${clothesData.url}`)}
+            alt={clothesData.clothesId}
+            style={{
+              width: '100px',
+              height: '100px',
+            }}
+            // onClick={() => setSelectedImg()}
+          />
+        ))}
+      </div>
     </div>
   );
 }
