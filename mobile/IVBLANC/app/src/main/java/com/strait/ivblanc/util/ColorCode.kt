d6f1186925b4295263c9dbe942ab4c48c4db573e
김민수 <ms001118@naver.com>
2022-02-05 12:45:34 +0900

    MO-407 ColorCode 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/util/ColorCode.kt
@@ -0,0 +1,7 @@
+package com.strait.ivblanc.util
+
+class ColorCode {
+    companion object {
+        val LIST = listOf("#ecc6c0", "#c14d49", "#e0633b", "#f6efdf", "#f6d766", "#35774d", "#cddeec", "#27476f", "#6d4859", "#abaeb6", "#5e4b48", "#9c7e3a", "#ffffff", "#000000", "etc", "variety")
+    }
+}
