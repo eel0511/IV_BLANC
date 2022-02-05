16d35192ce8e590868cc1bcab22e993dc92e3d19
SuhyungLee <eel0511@gmail.com>
2022-02-05 14:20:09 +0900

    MO-434 API response Fix

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/FriendApi.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/FriendApi.kt
@@ -1,7 +1,7 @@
 package com.strait.ivblanc.data.api
 
-import com.kakao.sdk.talk.model.Friend
 import com.strait.ivblanc.data.model.dto.FriendForUpload
+import com.strait.ivblanc.data.model.response.FriendListResponse
 import com.strait.ivblanc.data.model.response.FriendResponse
 import retrofit2.Response
 import retrofit2.http.*
@@ -9,7 +9,7 @@ import retrofit2.http.*
 interface FriendApi {
 
     @GET("/api/friend/all")
-    suspend fun getAllFriends(@Query("applicant")applicant:String):Response<List<FriendResponse>>
+    suspend fun getAllFriends(@Query("applicant")applicant:String):Response<FriendListResponse>
 
     @DELETE("/api/friend/cancel")
     suspend fun cancelFriend(@Body friend: FriendForUpload):Response<FriendResponse>
@@ -18,16 +18,16 @@ interface FriendApi {
     suspend fun deleteFriend(@Body friend: FriendForUpload):Response<FriendResponse>
 
     @GET("/api/friend/friendrequest")
-    suspend fun getAllFriendRequest(@Query("applicant")applicant: String):Response<List<FriendResponse>>
+    suspend fun getAllFriendRequest(@Query("applicant")applicant: String):Response<FriendListResponse>
 
     @GET("/api/friend/isaccept")
-    suspend fun getAllAcceptFriend(@Query("applicant")applicant: String):Response<List<FriendResponse>>
+    suspend fun getAllAcceptFriend(@Query("applicant")applicant: String):Response<FriendListResponse>
 
     @POST("/api/friend/isaccept")
     suspend fun acceptFriend(@Body friend: FriendForUpload):Response<FriendResponse>
 
     @GET("/api/friend/isnotaccept")
-    suspend fun getAllNotAcceptFriend(@Query("applicant")applicant: String):Response<List<FriendResponse>>
+    suspend fun getAllNotAcceptFriend(@Query("applicant")applicant: String):Response<FriendListResponse>
 
     @POST("/api/friend/request")
     suspend fun requestFriend(@Body friend: FriendForUpload):Response<FriendResponse>
