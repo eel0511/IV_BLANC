f6dac8ff2e1534aea88bd3b02b575e3d0d1d3dba
SuhyungLee <eel0511@gmail.com>
2022-02-05 14:33:53 +0900

    MO-437 친구 리스트 불러옴

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/main/FriendFragment.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/main/FriendFragment.kt
@@ -6,18 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.strait.ivblanc.R
 import com.strait.ivblanc.adapter.FriendRecyclerViewAdapter
 import com.strait.ivblanc.config.BaseFragment
 import com.strait.ivblanc.data.model.dto.FriendViewdata
+import com.strait.ivblanc.data.model.viewmodel.MainViewModel
+import com.strait.ivblanc.data.repository.FriendRepository
 import com.strait.ivblanc.databinding.FragmentFriendBinding
 import com.strait.ivblanc.databinding.FragmentHistoryBinding
+import kotlinx.coroutines.*
 
 
 class FriendFragment : BaseFragment<FragmentFriendBinding>(FragmentFriendBinding::bind,R.layout.fragment_friend){
 
     lateinit var friendRecyclerViewAdapter:FriendRecyclerViewAdapter
+    val friendRepository = FriendRepository()
+    val scope = CoroutineScope(Dispatchers.Main)
+    private val viewModel: MainViewModel by activityViewModels()
     private val itemClickListener = object :FriendRecyclerViewAdapter.ItemClickListener{
         override fun onClick(friendViewdata: FriendViewdata) {
             TODO("Not yet implemented")
@@ -30,11 +38,19 @@ class FriendFragment : BaseFragment<FragmentFriendBinding>(FragmentFriendBinding
 
     override fun onResume() {
         super.onResume()
-        reloadImages()
+        viewModel.setToolbarTitle("친구")
+        scope.launch {
+            reloadImages()
+        }
     }
-    fun reloadImages() {
+    suspend fun reloadImages() {
+        var response = friendRepository.getAllFriends("ssu@a.com")
         var u = Uri.parse("https://storage.googleapis.com/iv-blanc.appspot.com/00e3e841-0ec1-4261-909a-52ff448af69a.jpeg")
-        friendRecyclerViewAdapter.friendViewdata = listOf(FriendViewdata("",u,u,u,u,u,u,u,u))
+        var list = arrayListOf<FriendViewdata>()
+        response.data!!.dataSet!!.forEach {
+            list.add(FriendViewdata(it.friendName,u,u,u,u,u,u,u,u))
+        }
+        friendRecyclerViewAdapter.friendViewdata = list
         friendRecyclerViewAdapter.notifyDataSetChanged()
     }
     fun init(){
@@ -43,7 +59,7 @@ class FriendFragment : BaseFragment<FragmentFriendBinding>(FragmentFriendBinding
         }
         binding.friendRecyclerview.apply {
             adapter=friendRecyclerViewAdapter
-            layoutManager = LinearLayoutManager(requireActivity(),LinearLayoutManager.HORIZONTAL,false)
+            layoutManager = LinearLayoutManager(requireActivity(),LinearLayoutManager.VERTICAL,false)
         }
     }
 }
