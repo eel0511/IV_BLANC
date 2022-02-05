9f5dd233a6b30fa5be5ea14a60cbf5baff7537f1
SuhyungLee <eel0511@gmail.com>
2022-02-05 14:18:59 +0900

    MO-435 Repo 조건수정

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/FriendRepository.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/FriendRepository.kt
@@ -3,6 +3,7 @@ package com.strait.ivblanc.data.repository
 import com.strait.ivblanc.config.ApplicationClass
 import com.strait.ivblanc.data.api.FriendApi
 import com.strait.ivblanc.data.model.dto.FriendForUpload
+import com.strait.ivblanc.data.model.response.FriendListResponse
 import com.strait.ivblanc.data.model.response.FriendResponse
 import com.strait.ivblanc.util.Resource
 import com.strait.ivblanc.util.StatusCode
@@ -11,11 +12,11 @@ import java.lang.Exception
 class FriendRepository {
     val friendApi = ApplicationClass.sRetrofit.create(FriendApi::class.java)
 
-    suspend fun getAllFriends(applicant:String): Resource<List<FriendResponse>> {
+    suspend fun getAllFriends(applicant:String): Resource<FriendListResponse> {
         return try{
             val response=friendApi.getAllFriends(applicant)
             if(response.isSuccessful) {
-                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.containsKey("clothes_id")) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.isNotEmpty()) {
                     Resource.success(response.body()!!)
                 } else {
                     Resource.error(null, "알 수 없는 에러입니다.")
@@ -32,7 +33,7 @@ class FriendRepository {
         return try{
             val response=friendApi.cancelFriend(friend)
             if(response.isSuccessful) {
-                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.containsKey("clothes_id")) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.data!!.friendEmail.isNotEmpty()) {
                     Resource.success(response.body()!!)
                 } else {
                     Resource.error(null, "알 수 없는 에러입니다.")
@@ -49,7 +50,7 @@ class FriendRepository {
         return try{
             val response=friendApi.deleteFriend(friend)
             if(response.isSuccessful) {
-                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.containsKey("clothes_id")) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.data!!.friendEmail.isNotEmpty()) {
                     Resource.success(response.body()!!)
                 } else {
                     Resource.error(null, "알 수 없는 에러입니다.")
@@ -62,11 +63,11 @@ class FriendRepository {
         }
     }
 
-    suspend fun getAllFriendRequest(applicant:String): Resource<List<FriendResponse>> {
+    suspend fun getAllFriendRequest(applicant:String): Resource<FriendListResponse> {
         return try{
             val response=friendApi.getAllFriendRequest(applicant)
             if(response.isSuccessful) {
-                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.containsKey("clothes_id")) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.isNotEmpty()) {
                     Resource.success(response.body()!!)
                 } else {
                     Resource.error(null, "알 수 없는 에러입니다.")
@@ -78,11 +79,11 @@ class FriendRepository {
             Resource.error(null, "네트워크 상태를 확인해 주세요.")
         }
     }
-    suspend fun getAllAcceptFriend(applicant:String): Resource<List<FriendResponse>> {
+    suspend fun getAllAcceptFriend(applicant:String): Resource<FriendListResponse> {
         return try{
             val response=friendApi.getAllAcceptFriend(applicant)
             if(response.isSuccessful) {
-                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.containsKey("clothes_id")) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.isNotEmpty()) {
                     Resource.success(response.body()!!)
                 } else {
                     Resource.error(null, "알 수 없는 에러입니다.")
@@ -98,7 +99,7 @@ class FriendRepository {
         return try{
             val response=friendApi.acceptFriend(friend)
             if(response.isSuccessful) {
-                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.containsKey("clothes_id")) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.data!!.friendEmail.isNotEmpty()) {
                     Resource.success(response.body()!!)
                 } else {
                     Resource.error(null, "알 수 없는 에러입니다.")
@@ -110,11 +111,11 @@ class FriendRepository {
             Resource.error(null, "네트워크 상태를 확인해 주세요.")
         }
     }
-    suspend fun getAllNotAcceptFriend(applicant:String): Resource<List<FriendResponse>> {
+    suspend fun getAllNotAcceptFriend(applicant:String): Resource<FriendListResponse> {
         return try{
             val response=friendApi.getAllNotAcceptFriend(applicant)
             if(response.isSuccessful) {
-                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.containsKey("clothes_id")) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.isNotEmpty()) {
                     Resource.success(response.body()!!)
                 } else {
                     Resource.error(null, "알 수 없는 에러입니다.")
@@ -130,7 +131,7 @@ class FriendRepository {
         return try{
             val response=friendApi.requestFriend(friend)
             if(response.isSuccessful) {
-                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.dataSet!!.containsKey("clothes_id")) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1 && response.body()!!.data!!.friendEmail.isNotEmpty()) {
                     Resource.success(response.body()!!)
                 } else {
                     Resource.error(null, "알 수 없는 에러입니다.")
