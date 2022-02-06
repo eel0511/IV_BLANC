2e17c28efdc388a06e7988144d19ea706d6b9664
SuhyungLee <eel0511@gmail.com>
2022-02-06 18:52:26 +0900

    MO-435 bottom_navi , toolbar 먹통

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/friend/FriendCloset.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/friend/FriendCloset.kt
@@ -3,6 +3,8 @@ package com.strait.ivblanc.src.friend
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -20,40 +22,45 @@ import com.strait.ivblanc.src.photoSelect.PhotoSelectActivity
 import com.strait.ivblanc.ui.PhotoListFragment
 import com.strait.ivblanc.util.CategoryCode
 
-class FriendCloset :  BaseActivity<ActivityFriendClosetBinding>(ActivityFriendClosetBinding::inflate) {
-    val friendViewModel:FriendViewModel by viewModels()
+class FriendCloset :
+    BaseActivity<ActivityFriendClosetBinding>(ActivityFriendClosetBinding::inflate) {
+    val friendViewModel: FriendViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend_closet)
         friendViewModel.getAllClothesWithCategory(CategoryCode.TOTAL)
         init()
     }
+
     private fun init() {
         setToolbar()
         // 첫 화면 세팅
-        setFragment(FriendFragment(), "clothes")
+        setFragment(FriendFragment(), "friend_clothes")
 
         // nav click 시 fragment 변경
         binding.bottomNavFriend.setOnItemSelectedListener { item ->
-            when(item.itemId) {
+            when (item.itemId) {
                 R.id.nav_friend_clothes -> {
-                    setFragment(PhotoListFragment<Clothes>(), "clothes")
+                    Log.d("nav_c", "init: ")
+                    setFragment(PhotoListFragment<Clothes>(), "friend_clothes")
                     true
                 }
                 // Style로 변경
                 R.id.nav_friend_style -> {
-                    setFragment(FriendFragment(), "style")
+                    Log.d("nav_s", "init: ")
+                    setFragment(FriendFragment(), "friend_style")
                     true
                 }
                 else -> false
             }
         }
     }
+
     private fun setToolbar() {
-        setSupportActionBar(binding.toolbarFriend.toolbar)
+        setSupportActionBar(binding.toolbarFriend.friendToolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         friendViewModel.toolbarTitle.observe(this) {
-            Log.d("FriendCloset", "setToolbar: "+it)
+            Log.d("FriendCloset", "setToolbar: " + it)
             setToolbarTitle(it)
         }
         friendViewModel.leadingIconDrawable.observe(this) {
@@ -63,38 +70,45 @@ class FriendCloset :  BaseActivity<ActivityFriendClosetBinding>(ActivityFriendCl
         friendViewModel.trailingIconDrawable.observe(this) {
             setTrailingIcon(it, getListener(it))
         }
-    }    private fun setFragment(fragment: Fragment, tag: String) {
-        supportFragmentManager.beginTransaction().replace(R.id.frameLayout_friend_container, fragment, tag).commit()
+    }
+
+    private fun setFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout_friend_container, fragment, tag).commit()
     }
 
     // imageView의 background drawable Id에 따라 버튼 리스너 반환
     private fun getListener(resId: Int): View.OnClickListener {
-        return when(resId) {
-            R.drawable.ic_close -> object: View.OnClickListener {
+        return when (resId) {
+            R.drawable.ic_close -> object : View.OnClickListener {
                 override fun onClick(v: View?) {
                     this@FriendCloset.onBackPressed()
                 }
             }
             // 같은 add Drawable 일때, 현재 fragment의 tag로 리스너 설정
             R.drawable.ic_add -> {
-                when(getCurrentFragmentTag()) {
-                    "clothes" -> {
-                        object: View.OnClickListener {
+                when (getCurrentFragmentTag()) {
+                    "friend_clothes" -> {
+                        object : View.OnClickListener {
                             override fun onClick(v: View?) {
-                                startActivity(Intent(this@FriendCloset, PhotoSelectActivity::class.java).apply {
-                                    putExtra("intend", PhotoSelectActivity.CLOTHES)
-                                })
+                                startActivity(
+                                    Intent(
+                                        this@FriendCloset,
+                                        PhotoSelectActivity::class.java
+                                    ).apply {
+                                        putExtra("intend", PhotoSelectActivity.CLOTHES)
+                                    })
                             }
                         }
                     }
-                    "style" -> {
-                        object: View.OnClickListener {
+                    "friend_style" -> {
+                        object : View.OnClickListener {
                             override fun onClick(v: View?) {
                                 toast("스타일 생성 Activity로 이동", Toast.LENGTH_SHORT)
                             }
                         }
                     }
-                    else -> View.OnClickListener{}
+                    else -> View.OnClickListener {}
                 }
 
 
@@ -103,7 +117,8 @@ class FriendCloset :  BaseActivity<ActivityFriendClosetBinding>(ActivityFriendCl
         }
     }
 
-    private fun getCurrentFragmentTag(): String? = supportFragmentManager.findFragmentById(R.id.frameLayout_friend_container)?.tag
+    private fun getCurrentFragmentTag(): String? =
+        supportFragmentManager.findFragmentById(R.id.frameLayout_friend_container)?.tag
 
     private fun setToolbarTitle(title: String) {
         binding.toolbarFriend.textViewFriendToolbar.text = title
@@ -111,15 +126,19 @@ class FriendCloset :  BaseActivity<ActivityFriendClosetBinding>(ActivityFriendCl
 
     private fun setLeadingIcon(resId: Int, clickListener: View.OnClickListener) {
         try {
-            binding.toolbarFriend.imageViewFriendToolbarLeadingIcon.background = ResourcesCompat.getDrawable(resources, resId, null)
-        } catch (e: Exception) {}
+            binding.toolbarFriend.imageViewFriendToolbarLeadingIcon.background =
+                ResourcesCompat.getDrawable(resources, resId, null)
+        } catch (e: Exception) {
+        }
         binding.toolbarFriend.imageViewFriendToolbarLeadingIcon.setOnClickListener(clickListener)
     }
 
     private fun setTrailingIcon(resId: Int, clickListener: View.OnClickListener) {
         try {
-            binding.toolbarFriend.imageViewFriendToolbarTrailingIcon.background = ResourcesCompat.getDrawable(resources, resId, null)
-        } catch (e: Exception) {}
+            binding.toolbarFriend.imageViewFriendToolbarTrailingIcon.background =
+                ResourcesCompat.getDrawable(resources, resId, null)
+        } catch (e: Exception) {
+        }
         binding.toolbarFriend.imageViewFriendToolbarTrailingIcon.setOnClickListener(clickListener)
     }
 
