f78deac9ffb69c06e55118d79c0c1644e0d3e34e
이인섭 <inseob135@gmail.com>
2022-02-06 23:02:42 +0900

    FE-368 스타일 저장, 초기화 버튼 생성

--- web/src/layouts/MyStyle/MyStyleTopbar.js
+++ web/src/layouts/MyStyle/MyStyleTopbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Nav, NavDropdown } from 'react-bootstrap';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import axios from 'axios';
 
 import SelectedImage from '../../components/MyStyle/SelectedImage';
@@ -252,6 +254,19 @@ export default function MyStyleTopbar() {
       </div> */}
 
       <SelectedImage selectedClothes={selectedClothes} />
+
+      {selectedClothes.length > 0 && (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Stack direction='row' spacing={2}>
+            <Button variant='contained' color='success'>
+              스타일 저장
+            </Button>
+            <Button variant='contained' color='error'>
+              초기화
+            </Button>
+          </Stack>
+        </div>
+      )}
     </div>
   );
 }
