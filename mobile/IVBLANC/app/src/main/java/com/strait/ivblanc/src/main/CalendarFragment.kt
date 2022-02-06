3237a9fa5d93045e91b6fba7ca3a700f7d003da3
김민수 <ms001118@naver.com>
2022-02-07 02:34:49 +0900

    MO-380 CalendarFragment 화면 구성, viewpager 세팅

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/main/CalendarFragment.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/main/CalendarFragment.kt
@@ -1,14 +1,66 @@
 package com.strait.ivblanc.src.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.strait.ivblanc.R
 import com.strait.ivblanc.config.BaseFragment
 import com.strait.ivblanc.databinding.FragmentCalendarBinding
+import com.strait.ivblanc.ui.CalendarMonthFragment
+import org.joda.time.DateTime
 
+private const val TAG = "CalendarFragment_debuk"
 class CalendarFragment : BaseFragment<FragmentCalendarBinding>(FragmentCalendarBinding::bind, R.layout.fragment_calendar) {
+    lateinit var viewPager: ViewPager2
+    private val now = DateTime.now()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
 
+    private fun init() {
+        setViewPager()
+        setUiAfterSetViewPager()
+    }
+
+    private fun setViewPager() {
+        viewPager = binding.viewpagerCalendarF
+        viewPager.adapter = ViewPagerAdapter(requireActivity())
+        viewPager.setCurrentItem(Int.MAX_VALUE, true)
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                val date = now.minusMonths(Int.MAX_VALUE - position - 1)
+                binding.textViewCalendarFDate.text = "${date.year().get()}.${date.monthOfYear}"
+            }
+        })
+    }
+
+    private fun setUiAfterSetViewPager() {
+        binding.textViewCalendarFDate.text = "${now.year().get()}.${now.monthOfYear}"
+        binding.imageViewCalendarFBack.setOnClickListener {
+            if(viewPager.currentItem > 0) {
+                viewPager.setCurrentItem(viewPager.currentItem - 1)
+            }
+        }
+        binding.imageViewCalendarFNext.setOnClickListener {
+            if(viewPager.currentItem < Int.MAX_VALUE - 1) {
+                viewPager.setCurrentItem(viewPager.currentItem + 1)
+            }
+        }
+    }
+
+    inner class ViewPagerAdapter(fa: FragmentActivity): FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = Int.MAX_VALUE
+
+        override fun createFragment(position: Int): Fragment {
+            return CalendarMonthFragment(now.minusMonths(Int.MAX_VALUE - position - 1))
+        }
+    }
 }
