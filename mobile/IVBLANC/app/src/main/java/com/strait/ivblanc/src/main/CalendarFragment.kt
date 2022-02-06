3c28206fe901e0cf95e2100c6afbea008a0e63f0
김민수 <ms001118@naver.com>
2022-02-06 21:09:18 +0900

    MO-395 CalendarFragment 생성

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/main/CalendarFragment.kt
@@ -0,0 +1,14 @@
+package com.strait.ivblanc.src.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.strait.ivblanc.R
+import com.strait.ivblanc.config.BaseFragment
+import com.strait.ivblanc.databinding.FragmentCalendarBinding
+
+class CalendarFragment : BaseFragment<FragmentCalendarBinding>(FragmentCalendarBinding::bind, R.layout.fragment_calendar) {
+
+}
