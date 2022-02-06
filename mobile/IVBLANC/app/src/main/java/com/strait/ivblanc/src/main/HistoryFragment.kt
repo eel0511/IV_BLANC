2a3d389134f7daf920bbcfe6f72fa792b4a6889f
김민수 <ms001118@naver.com>
2022-02-06 17:56:54 +0900

    MO-382 viewpager, tablayout 세팅

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/main/HistoryFragment.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/main/HistoryFragment.kt
@@ -5,9 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.strait.ivblanc.R
 import com.strait.ivblanc.config.BaseFragment
+import com.strait.ivblanc.data.model.viewmodel.MainViewModel
 import com.strait.ivblanc.databinding.FragmentHistoryBinding
+import com.strait.ivblanc.src.photoSelect.AlbumFragment
+import com.strait.ivblanc.src.photoSelect.CameraFragment
 
 class HistoryFragment : BaseFragment<FragmentHistoryBinding>(FragmentHistoryBinding::bind, R.layout.fragment_history) {
+    private val mainViewModel: MainViewModel by activityViewModels()
+    private lateinit var viewPager: ViewPager2
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        viewPager = binding.viewpagerHistoryF
+        viewPager.adapter = ViewPagerAdapter(requireActivity())
+        // 스와이핑 입력 무시
+        viewPager.isUserInputEnabled = false
+        TabLayoutMediator(binding.tabLayoutHistoryF, viewPager) { tab, position ->
+            when(position) {
+                0 -> tab.text = resources.getString(R.string.calendar)
+                else -> tab.text = resources.getString(R.string.map)
+            }
+        }.attach()
+    }
+
+    private inner class ViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = 2
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                // TODO: 2022/02/06 캘린더 fragment로 이동
+                0 -> {
+                    toast("캘린더 fragment", Toast.LENGTH_SHORT)
+                    AlbumFragment()
+                }
+                // TODO: 2022/02/06 지도 fragment로 이동
+                else -> {
+                    toast("지도 fragment", Toast.LENGTH_SHORT)
+                    CameraFragment()
+                }
+            }
+        }
+    }
+
 }
