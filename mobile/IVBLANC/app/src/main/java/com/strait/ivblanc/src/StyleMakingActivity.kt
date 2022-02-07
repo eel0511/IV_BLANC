ee506cad0c1a337738f12406858a23dac7af6950
김민수 <ms001118@naver.com>
2022-02-07 14:14:04 +0900

    MO-456 StyleMakingActivity 생성

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/StyleMakingActivity.kt
@@ -0,0 +1,12 @@
+package com.strait.ivblanc.src
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.strait.ivblanc.config.BaseActivity
+import com.strait.ivblanc.databinding.ActivityStyleMakingBinding
+
+class StyleMakingActivity : BaseActivity<ActivityStyleMakingBinding>(ActivityStyleMakingBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
