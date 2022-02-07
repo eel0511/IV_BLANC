cb745e8e354b0e0f93e97efea605173831b9e196
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-02-07 13:18:02 +0900

    FE-393 친구 리스트 친구 옷 조회 서버연동

--- web/src/components/Friends/FriendsListItem.js
+++ web/src/components/Friends/FriendsListItem.js
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Avatar, Paper, Box } from '@mui/material';
+import './Friends.css';
 
-export default function FirendsListItem() {
+export default function FirendsListItem({ friend }) {
+  const [friendsClothesList, setFriendsClothesList] = useState([
+    {
+      "category": 0,
+      "clothesId": 0,
+      "color": "string",
+      "count": 0,
+      "createDate": "2022-02-07T02:00:58.443Z",
+      "dislikePoint": 0,
+      "favorite": 0,
+      "likePoint": 0,
+      "material": 0,
+      "season": 0,
+      "size": 0,
+      "updateDate": "2022-02-07T02:00:58.443Z",
+      "url": "string",
+      "userId": 0
+    }
+  ]);
+
+  const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJQayI6IjIiLCJpYXQiOjE2NDM4NTQ1MDIsImV4cCI6MTY0NjQ0NjUwMn0.s4B6viyO_tR8lZMUdxW62u82uT08ZltwgEBpuvTBZOQ';
+  const getFriendsClothesList = () => {
+    axios
+      .get('http://i6d104.p.ssafy.io:9999/api/clothes/friendclothes',
+      { 
+        headers: {
+          'X-AUTH-TOKEN': `${token}`
+        },
+        params: {
+          email: 'aaa@bbb.com'
+        }
+      })
+      .then((response) => {
+        // setFriendsClothesList(response.data.data);
+        console.log(response.data.data)
+      });
+  };
+
+  // useEffect(() => {
+  //   getFriendsClothesList();
+  // }, []);
+
   return (
     <>
+      <div className='friend__header'>
+        <Avatar
+          className='friend__avatar'
+          alt=''
+          src='/static/images/avatar/1.jpg'
+        />
+        <h3>{friend.friendName}</h3>
+      </div>
+      <div className='friend__body'>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: 1,
+              width: 128,
+              height: 128,
+            },
+          }}
+        >
+          <Paper elevation={8} />
+          <Paper elevation={8} />
+          <Paper elevation={8} />
+          <Paper elevation={8} />
+          <Paper elevation={8} />
+        </Box>
+      </div>
     </>
   );
 }
--- web/src/layouts/Friends/FriendsList.js
+++ web/src/layouts/Friends/FriendsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Avatar, Paper, Box } from '@mui/material';
 import './Friends.css';
+import FirendsListItem from '../../components/Friends/FriendsListItem';
 
 export default function FriendsList() {
   const [friendsList, setFriendsList] = useState([
@@ -18,24 +19,6 @@ export default function FriendsList() {
       friendName: '장나박이'
     }
   ]);
-  const [friendsClothesList, setFriendsClothesList] = useState([
-    {
-      "category": 0,
-      "clothesId": 0,
-      "color": "string",
-      "count": 0,
-      "createDate": "2022-02-07T02:00:58.443Z",
-      "dislikePoint": 0,
-      "favorite": 0,
-      "likePoint": 0,
-      "material": 0,
-      "season": 0,
-      "size": 0,
-      "updateDate": "2022-02-07T02:00:58.443Z",
-      "url": "string",
-      "userId": 0
-    }
-  ]);
 
   const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJQayI6IjIiLCJpYXQiOjE2NDM4NTQ1MDIsImV4cCI6MTY0NjQ0NjUwMn0.s4B6viyO_tR8lZMUdxW62u82uT08ZltwgEBpuvTBZOQ';
   const getFriendsList = () => {
@@ -50,8 +33,8 @@ export default function FriendsList() {
         }
       })
       .then((response) => {
-        // setFriendsList(response.data);
-        console.log(response.data.data)
+        setFriendsList(response.data.data);
+        // console.log(response.data.data)
       });
   };
 
@@ -61,36 +44,11 @@ export default function FriendsList() {
 
   return (
     <div className='friend'>
-      {/* 친구 프로필 이름, 친구옷 스타일링 */}
-      <div className='friend__header'>
-        <Avatar
-          className='friend__avatar'
-          alt=''
-          src='/static/images/avatar/1.jpg'
-        />
-        <h3>friendName</h3>
+      {friendsList.map((friend, id) => (
+      <div key={id}>
+        <FirendsListItem friend={friend}/>
       </div>
-
-      {/* 친구 옷 */}
-      <div className='friend__body'>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            '& > :not(style)': {
-              m: 1,
-              width: 128,
-              height: 128,
-            },
-          }}
-        >
-          <Paper elevation={8} />
-          <Paper elevation={8} />
-          <Paper elevation={8} />
-          <Paper elevation={8} />
-          <Paper elevation={8} />
-        </Box>
-      </div>
+      ))}
     </div>
   );
 }
