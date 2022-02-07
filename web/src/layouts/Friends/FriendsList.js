1fad46c03bbc7f06630de50a39c01903e6c44f24
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-02-07 11:23:09 +0900

    FE-389 친구 리스트 UI 구조 생성

--- web/src/layouts/Friends/FriendsList.js
+++ web/src/layouts/Friends/FriendsList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Avatar, Paper, Box } from '@mui/material';
+import './Friends.css';
 
 export default function FriendsList() {
   const [friendsList, setFriendsList] = useState([
@@ -14,6 +16,24 @@ export default function FriendsList() {
     {
       friendEmail: 'aaa@bbb.com',
       friendName: '장나박이'
+    }
+  ]);
+  const [friendsClothesList, setFriendsClothesList] = useState([
+    {
+      "category": 0,
+      "clothesId": 0,
+      "color": "string",
+      "count": 0,
+      "createDate": "2022-02-07T02:00:58.443Z",
+      "dislikePoint": 0,
+      "favorite": 0,
+      "likePoint": 0,
+      "material": 0,
+      "season": 0,
+      "size": 0,
+      "updateDate": "2022-02-07T02:00:58.443Z",
+      "url": "string",
+      "userId": 0
     }
   ]);
 
@@ -40,8 +60,37 @@ export default function FriendsList() {
   // }, []);
 
   return (
-    <>
-      
-    </>
+    <div className='friend'>
+      {/* 친구 프로필 이름, 친구옷 스타일링 */}
+      <div className='friend__header'>
+        <Avatar
+          className='friend__avatar'
+          alt=''
+          src='/static/images/avatar/1.jpg'
+        />
+        <h3>friendName</h3>
+      </div>
+
+      {/* 친구 옷 */}
+      <div className='friend__body'>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: 1,
+              width: 128,
+              height: 128,
+            },
+          }}
+        >
+          <Paper elevation={8} />
+          <Paper elevation={8} />
+          <Paper elevation={8} />
+          <Paper elevation={8} />
+          <Paper elevation={8} />
+        </Box>
+      </div>
+    </div>
   );
 }
