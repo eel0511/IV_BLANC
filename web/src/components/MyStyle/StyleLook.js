4e4b93b43ae38c9202877b6241d24ad620ade429
이인섭 <inseob135@gmail.com>
2022-02-07 14:37:03 +0900

    FE-369 룩 보여주는 컴포넌트 생성 및 임시 데이터 표시

--- web/src/components/MyStyle/StyleLook.js
+++ web/src/components/MyStyle/StyleLook.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-function StyleLook() {
-  return <div>스타일 룩</div>;
+function StyleLook({ selectedClothes }) {
+  return (
+    <div className='wrapper'>
+      <div className='col'>
+        {selectedClothes.map((clothesData) => (
+          <div className='look' key={clothesData.clothesId}>
+            <img
+              className='MyStyleClothesItemImg'
+              src={require(`../../assets/${clothesData.url}`)}
+              // src={clothesData.url}
+              alt={clothesData.clothesId}
+              style={{
+                maxWidth: '100%',
+                maxHeight: '100%',
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default StyleLook;
