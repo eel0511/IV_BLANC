64a97874bdcd5976b4cd75d9d27a383385c2210e
이인섭 <inseob135@gmail.com>
2022-02-07 17:49:49 +0900

    FE-369 useEffect state 값 설정 for, switch 문으로 변경

--- web/src/components/MyStyle/StyleLook.js
+++ web/src/components/MyStyle/StyleLook.js
@@ -11,19 +11,43 @@ function StyleLook({ selectedClothes }) {
 
   useEffect(() => {
     // 값이 없다면 undefined가 뜬다.
-    setShirts(selectedClothes.find((clothes) => clothes.category === 1));
-    setPants(selectedClothes.find((clothes) => clothes.category === 2));
-    setOuter(selectedClothes.find((clothes) => clothes.category === 3));
-    setShoes(selectedClothes.find((clothes) => clothes.category === 4));
-    setBag(selectedClothes.find((clothes) => clothes.category === 5));
-    setHat(selectedClothes.find((clothes) => clothes.category === 6));
-    setOthers(selectedClothes.find((clothes) => clothes.category === 7));
+    for (let i = 0; i < selectedClothes.length; ++i) {
+      switch (selectedClothes[i].category) {
+        case 1:
+          setShirts(selectedClothes.find((clothes) => clothes.category === 1));
+          break;
+        case 2:
+          setPants(selectedClothes.find((clothes) => clothes.category === 2));
+          break;
+        case 3:
+          setOuter(selectedClothes.find((clothes) => clothes.category === 3));
+          break;
+        case 4:
+          setShoes(selectedClothes.find((clothes) => clothes.category === 4));
+          break;
+        case 5:
+          setBag(selectedClothes.find((clothes) => clothes.category === 5));
+          break;
+        case 6:
+          setHat(selectedClothes.find((clothes) => clothes.category === 6));
+          break;
+        default:
+          setOthers(selectedClothes.find((clothes) => clothes.category === 7));
+      }
+    }
+    // setShirts(selectedClothes.find((clothes) => clothes.category === 1));
+    // setPants(selectedClothes.find((clothes) => clothes.category === 2));
+    // setOuter(selectedClothes.find((clothes) => clothes.category === 3));
+    // setShoes(selectedClothes.find((clothes) => clothes.category === 4));
+    // setBag(selectedClothes.find((clothes) => clothes.category === 5));
+    // setHat(selectedClothes.find((clothes) => clothes.category === 6));
+    // setOthers(selectedClothes.find((clothes) => clothes.category === 7));
   }, []);
 
   return (
     <div className='wrapper'>
       <div className='col'>
-        {selectedClothes.map((clothesData) => (
+        {/* {selectedClothes.map((clothesData) => (
           <div className='look' key={clothesData.clothesId}>
             <img
               className='MyStyleClothesItemImg'
@@ -36,7 +60,7 @@ function StyleLook({ selectedClothes }) {
               }}
             />
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
