36c6349db873a3508f758ad6bdcb8367d474d65b
김현수 <hskim9706@gmail.com>
2022-02-08 12:51:30 +0900

    BE-339 테스트 완료

--- server/api-module/src/main/java/com/ivblanc/api/dto/req/UpdatePwReqDTO.java
+++ server/api-module/src/main/java/com/ivblanc/api/dto/req/UpdatePwReqDTO.java
@@ -17,7 +17,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UpdatePwReqDTO {
 	@NotBlank(message = "이메일은 필수 입력값입니다.")
-	@Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식을 확인해주세요.")
 	@ApiModelProperty(value = "이메일", required = true, example = "123@ssafy.com")
 	private String email;
 
