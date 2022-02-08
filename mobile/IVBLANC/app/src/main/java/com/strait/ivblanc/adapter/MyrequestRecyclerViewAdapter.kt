fa48a4d8a27433a0593abbbd4bc619bf69d19eb4
SuhyungLee <eel0511@gmail.com>
2022-02-08 13:01:48 +0900

    MO-00 SAVE

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/MyrequestRecyclerViewAdapter.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/MyrequestRecyclerViewAdapter.kt
@@ -1,9 +1,11 @@
 package com.strait.ivblanc.adapter
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.strait.ivblanc.R
@@ -14,9 +16,11 @@ class MyrequestRecyclerViewAdapter():RecyclerView.Adapter<MyrequestRecyclerViewA
     var myrequestList = listOf<String>()
 
     inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        private val img = itemView.findViewById<ImageView>(R.id.myrequest_img)
         private val text = itemView.findViewById<TextView>(R.id.myreques_text)
         private val button = itemView.findViewById<ImageButton>(R.id.my_request_button)
         fun bind(item:String){
+            img.setImageURI( Uri.parse("https://storage.googleapis.com/iv-blanc.appspot.com/00e3e841-0ec1-4261-909a-52ff448af69a.jpeg"))
             text.text = item+"님이 친구 요청을 보내셨습니다."
             button.setOnClickListener {
                 itemClickListener.onClick()
