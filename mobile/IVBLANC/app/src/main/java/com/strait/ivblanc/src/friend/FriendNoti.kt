a29955487c0d6a5441d167e5ac4a02b91cabaf86
SuhyungLee <eel0511@gmail.com>
2022-02-08 14:12:30 +0900

    MO-449 noti activity

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/friend/FriendNoti.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/friend/FriendNoti.kt
@@ -1,55 +1,81 @@
 package com.strait.ivblanc.src.friend
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
-import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.strait.ivblanc.R
-import com.strait.ivblanc.adapter.FriendRecyclerViewAdapter
 import com.strait.ivblanc.adapter.MyrequestRecyclerViewAdapter
+import com.strait.ivblanc.adapter.WaitRecyclerViewAdapter
 import com.strait.ivblanc.config.BaseActivity
 import com.strait.ivblanc.data.model.dto.Friend
-import com.strait.ivblanc.data.model.dto.FriendViewdata
 import com.strait.ivblanc.data.model.viewmodel.FriendViewModel
 import com.strait.ivblanc.databinding.ActivityFriendNotiBinding
-import com.strait.ivblanc.databinding.ActivityMainBinding
 
 class FriendNoti : BaseActivity<ActivityFriendNotiBinding>(ActivityFriendNotiBinding::inflate) {
     private val friendViewModel: FriendViewModel by viewModels()
     lateinit var myrequestRecyclerViewAdapter: MyrequestRecyclerViewAdapter
-    var list = arrayListOf<String>()
-    private val itemClickListener = object : MyrequestRecyclerViewAdapter.ItemClickListener {
-        override fun onClick() {
+    lateinit var waitRecyclerViewAdapter: WaitRecyclerViewAdapter
+    var requestlist = arrayListOf<Friend>()
+    var waitlist = arrayListOf<Friend>()
+    private val myrequestitemClickListener =
+        object : MyrequestRecyclerViewAdapter.ItemClickListener {
+            override fun onClick(friend: Friend) {
+                friendViewModel.myacceptFriend(friend.friendEmail, "aaa@a.com")
+            }
+        }
+    private val waititemClickListener = object : WaitRecyclerViewAdapter.ItemClickListener {
+        override fun onClick(friend: Friend) {
 
         }
+
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         friendViewModel.getmyrequestFriend("aaa@a.com")
-        init()
+        friendViewModel.getmyWaitFriend("aaa@a.com")
+        initMyRequestRecycler()
+        initWaitRecycler()
     }
 
-    fun init(){
+    fun initMyRequestRecycler() {
         myrequestRecyclerViewAdapter = MyrequestRecyclerViewAdapter().apply {
-            itemClickListener = this@FriendNoti.itemClickListener
+            itemClickListener = this@FriendNoti.myrequestitemClickListener
         }
         binding.myrequestRecyclerView.apply {
             adapter = myrequestRecyclerViewAdapter
-            layoutManager= LinearLayoutManager(this@FriendNoti, LinearLayoutManager.VERTICAL, false)
+            layoutManager =
+                LinearLayoutManager(this@FriendNoti, LinearLayoutManager.VERTICAL, false)
 
         }
-        friendViewModel.friendRequestListLiveData.observe(this){
-            list.clear()
-            Log.d("ssss", "init: "+it)
+        friendViewModel.friendRequestListLiveData.observe(this) {
+            requestlist.clear()
+            Log.d("ssss", "init: " + it)
             it.forEach {
-                list.add(it.friendName)
+                requestlist.add(it)
             }
-            myrequestRecyclerViewAdapter.myrequestList=list
+            myrequestRecyclerViewAdapter.myrequestList = requestlist
             myrequestRecyclerViewAdapter.notifyDataSetChanged()
+        }
+    }
+
+    fun initWaitRecycler() {
+        waitRecyclerViewAdapter = WaitRecyclerViewAdapter().apply {
+            itemClickListener = this@FriendNoti.waititemClickListener
+        }
+        binding.waitRecycleView.apply {
+            adapter = waitRecyclerViewAdapter
+            layoutManager =
+                LinearLayoutManager(this@FriendNoti, LinearLayoutManager.VERTICAL, false)
+        }
+        friendViewModel.friendWaitListLiveData.observe(this) {
+            waitlist.clear()
+            it.forEach {
+                waitlist.add(it)
+            }
+            waitRecyclerViewAdapter.mywaitList = waitlist
+            waitRecyclerViewAdapter.notifyDataSetChanged()
         }
     }
 }
