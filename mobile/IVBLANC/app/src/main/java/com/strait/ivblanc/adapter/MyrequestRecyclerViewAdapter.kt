99d9f0e2bfd692a6ec91847e843fd3964b7aecbf
SuhyungLee <eel0511@gmail.com>
2022-02-08 14:12:12 +0900

    MO-449 RecyclerView type change

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/MyrequestRecyclerViewAdapter.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/MyrequestRecyclerViewAdapter.kt
@@ -9,21 +9,22 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.strait.ivblanc.R
+import com.strait.ivblanc.data.model.dto.Friend
 import com.strait.ivblanc.data.model.dto.FriendViewdata
 
 class MyrequestRecyclerViewAdapter():RecyclerView.Adapter<MyrequestRecyclerViewAdapter.ViewHolder>(){
     lateinit var itemClickListener: ItemClickListener
-    var myrequestList = listOf<String>()
+    var myrequestList = listOf<Friend>()
 
     inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
         private val img = itemView.findViewById<ImageView>(R.id.myrequest_img)
         private val text = itemView.findViewById<TextView>(R.id.myreques_text)
         private val button = itemView.findViewById<ImageButton>(R.id.my_request_button)
-        fun bind(item:String){
+        fun bind(item: Friend){
             img.setImageURI( Uri.parse("https://storage.googleapis.com/iv-blanc.appspot.com/00e3e841-0ec1-4261-909a-52ff448af69a.jpeg"))
-            text.text = item+"님이 친구 요청을 보내셨습니다."
+            text.text = item.friendName+"님이 친구 요청을 보내셨습니다."
             button.setOnClickListener {
-                itemClickListener.onClick()
+                itemClickListener.onClick(item)
             }
         }
     }
@@ -39,6 +40,6 @@ class MyrequestRecyclerViewAdapter():RecyclerView.Adapter<MyrequestRecyclerViewA
     override fun getItemCount(): Int = myrequestList.size
 
     interface ItemClickListener {
-        fun onClick()
+        fun onClick(friend: Friend)
     }
 }
