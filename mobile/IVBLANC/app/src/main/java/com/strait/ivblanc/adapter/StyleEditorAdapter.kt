9ef25d7158b00038cd0b8232d3066ac829949f2b
김민수 <ms001118@naver.com>
2022-02-08 10:35:48 +0900

    MO-456 containerView의 멀티 터치 이벤트 처리, imageView에 focusedImageView 설정하는 클릭리스너 세팅

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/StyleEditorAdapter.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/adapter/StyleEditorAdapter.kt
@@ -1,13 +1,17 @@
 package com.strait.ivblanc.adapter
 
+import android.content.Context
 import android.content.res.Resources
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.strait.ivblanc.data.model.dto.Clothes
+import com.strait.ivblanc.src.photoSelect.PhotoSelectActivity
 
+private const val TAG = "EditorAdapter_debuk"
 class StyleEditorAdapter(val containerView: ViewGroup) {
+    var focusedImageView: ImageView? = null
     // K = largeCategory, V = clothes
     // 상의, 하의 등 각 카테고리 마다 하나의 아이템만 존재
     private val clothesMap = mutableMapOf<Int, Clothes>()
@@ -15,8 +19,40 @@ class StyleEditorAdapter(val containerView: ViewGroup) {
     // 각 카테고리 아이템을 나타내는 이미지뷰
     private val imageViews = mutableMapOf<Int, ImageView>()
 
+    init {
+        val scaleGestureDetector = ScaleGestureDetector(containerView.context, object: ScaleGestureDetector.SimpleOnScaleGestureListener() {
+            override fun onScale(detector: ScaleGestureDetector?): Boolean {
+                Log.d(TAG, "onScale: ${focusedImageView?.id}")
+                val scaleFactor = focusedImageView?.scaleX
+                scaleFactor?.let {
+                    focusedImageView?.scaleY = scaleFactor * detector!!.scaleFactor
+                    focusedImageView?.scaleX = scaleFactor * detector!!.scaleFactor
+                }
+                return true
+            }
+        })
+
+        containerView.setOnTouchListener(object: View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                event?.let {
+                    if(event.pointerCount > 0) {
+                        scaleGestureDetector.onTouchEvent(event)
+                    }
+                }
+
+                return true
+            }
+
+        })
+    }
+
     fun addImageView(imageView: ImageView) {
-        imageViews[imageViews.size + 1] = imageView
+        imageViews[imageViews.size + 1] = imageView.apply {
+            setOnClickListener {
+                focusedImageView = this
+                Log.d(TAG, "addImageView: ${this.id}")
+            }
+        }
     }
 
     fun addOrUpdateClothes(clothes: Clothes) {
