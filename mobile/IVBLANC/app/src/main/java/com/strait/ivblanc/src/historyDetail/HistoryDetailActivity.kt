20c2151a8d1572a266736c5e37ddc811cb31ea5c
김현수 <hskim9706@gmail.com>
2022-02-09 15:22:09 +0900

    MO-390 HistoryDetailActivity 생성

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/historyDetail/HistoryDetailActivity.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/historyDetail/HistoryDetailActivity.kt
@@ -1,18 +1,11 @@
 package com.strait.ivblanc.src.historyDetail
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.strait.ivblanc.R
 import com.strait.ivblanc.config.BaseActivity
-import com.strait.ivblanc.data.model.dto.Clothes
 import com.strait.ivblanc.data.model.dto.History
-import com.strait.ivblanc.databinding.ActivityClothesDetailBinding
 import com.strait.ivblanc.databinding.ActivityHistoryDetailBinding
-import com.strait.ivblanc.util.CategoryCode
-import com.strait.ivblanc.util.MaterialCode
 
 class HistoryDetailActivity : BaseActivity<ActivityHistoryDetailBinding>(
     ActivityHistoryDetailBinding::inflate) {
@@ -27,11 +20,35 @@ class HistoryDetailActivity : BaseActivity<ActivityHistoryDetailBinding>(
     }
 
     private fun setClickListeners() {
-
+        binding.imageViewHistoryDetailClose.setOnClickListener {
+            finish()
+        }
+        binding.imageViewHistoryDetailEdit.setOnClickListener {
+            // TODO: 히스토리 수정 화면으로 이동
+        }
     }
 
     private fun setHistoryInfo() {
         Glide.with(this).load(history.styleUrl).into(binding.imageViewHistoryDetailStyle)
+
+        binding.textViewHistoryDetailDate.text = history.date
+        binding.textViewHistoryDetailSubject.text = history.subject
+        binding.textViewHistoryDetailLocation.text = "" // TODO: google map api로 위.경도에 따른 지역명 출력
+        binding.textViewHistoryDetailTemperature.text = history.temperature_high.toString() + "/" + history.temperature_low.toString()
+        binding.textViewHistoryDetailText.text = history.text
+
+        if(history.weather == "맑음"){
+            binding.imageViewHistoryDetailWeather.setImageResource(R.drawable.icon_weather_sunny);
+        } else if(history.weather == "흐림"){
+            binding.imageViewHistoryDetailWeather.setImageResource(R.drawable.icon_weather_cloudy);
+        } else if(history.weather == "눈"){
+            binding.imageViewHistoryDetailWeather.setImageResource(R.drawable.icon_weather_snowy);
+        } else if(history.weather == "비"){
+            binding.imageViewHistoryDetailWeather.setImageResource(R.drawable.icon_weather_rainy);
+        }
+    }
+
+    private fun setRecyclerView(){
 
     }
 }
