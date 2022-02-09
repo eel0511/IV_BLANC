4c777880931904c71d62f2abf38314d5302c2399
김현수 <hskim9706@gmail.com>
2022-02-09 23:54:16 +0900

    MO-390 역지오코딩 함수 구현

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/historyDetail/HistoryDetailActivity.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/historyDetail/HistoryDetailActivity.kt
@@ -1,15 +1,22 @@
 package com.strait.ivblanc.src.historyDetail
 
+import android.location.Geocoder
 import android.os.Bundle
 import com.bumptech.glide.Glide
-import com.strait.ivblanc.R
 import com.strait.ivblanc.config.BaseActivity
 import com.strait.ivblanc.data.model.dto.History
 import com.strait.ivblanc.databinding.ActivityHistoryDetailBinding
 
+import android.location.Address
+import android.util.Log
+
+import java.io.IOException
+
+
 class HistoryDetailActivity : BaseActivity<ActivityHistoryDetailBinding>(
     ActivityHistoryDetailBinding::inflate) {
     lateinit var history: History
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         intent.getParcelableExtra<History>("history")?.let {
@@ -33,19 +40,44 @@ class HistoryDetailActivity : BaseActivity<ActivityHistoryDetailBinding>(
 
         binding.textViewHistoryDetailDate.text = history.date
         binding.textViewHistoryDetailSubject.text = history.subject
-        binding.textViewHistoryDetailLocation.text = "" // TODO: google map api로 위.경도에 따른 지역명 출력
+        binding.textViewHistoryDetailLocation.text = getLocation()
         binding.textViewHistoryDetailTemperature.text = history.temperature_high.toString() + "/" + history.temperature_low.toString()
         binding.textViewHistoryDetailText.text = history.text
 
         if(history.weather == "맑음"){
-            binding.imageViewHistoryDetailWeather.setImageResource(R.drawable.icon_weather_sunny);
+            binding.imageViewHistoryDetailWeather.setImageResource(com.strait.ivblanc.R.drawable.icon_weather_sunny);
         } else if(history.weather == "흐림"){
-            binding.imageViewHistoryDetailWeather.setImageResource(R.drawable.icon_weather_cloudy);
+            binding.imageViewHistoryDetailWeather.setImageResource(com.strait.ivblanc.R.drawable.icon_weather_cloudy);
         } else if(history.weather == "눈"){
-            binding.imageViewHistoryDetailWeather.setImageResource(R.drawable.icon_weather_snowy);
+            binding.imageViewHistoryDetailWeather.setImageResource(com.strait.ivblanc.R.drawable.icon_weather_snowy);
         } else if(history.weather == "비"){
-            binding.imageViewHistoryDetailWeather.setImageResource(R.drawable.icon_weather_rainy);
+            binding.imageViewHistoryDetailWeather.setImageResource(com.strait.ivblanc.R.drawable.icon_weather_rainy);
         }
+    }
+
+    private fun getLocation() : String {
+
+        val geocoder = Geocoder(this)
+
+        var list: List<Address>? = null
+        try {
+            val latitude = history.latitude
+            val longitude = history.longitude
+            list = geocoder.getFromLocation(
+                latitude,  // 위도
+                longitude,  // 경도
+                10
+            ) // 얻어올 값의 개수
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e("test", "입출력 오류 - 서버에서 주소변환시 에러발생")
+        }
+        if (list != null) {
+            if (!list.isEmpty()) {
+                return list.get(0).toString()
+            }
+        }
+        return "알 수 없는 장소"
     }
 
     private fun setRecyclerView(){
