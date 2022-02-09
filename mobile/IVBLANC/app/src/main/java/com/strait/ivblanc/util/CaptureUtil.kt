eea07d49eeed77250b2d56d24758d67b45beb406
김민수 <ms001118@naver.com>
2022-02-09 23:17:35 +0900

    MO-478 view의 화면을 캡처하여 jpeg로 저장하는 CaptureUtil 생성

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/util/CaptureUtil.kt
@@ -0,0 +1,65 @@
+package com.strait.ivblanc.util
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.view.View
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CaptureUtil {
+    companion object {
+        fun saveCapture(view: View): Uri? {
+            val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            view.draw(canvas)
+
+            val currentMillis = System.currentTimeMillis()
+            val name = "ivblanc${SimpleDateFormat("yyyy-MM-ddThh:mm:ss", Locale.KOREA).format(currentMillis)}.jpg"
+            var fos: OutputStream?
+            var imageUri: Uri?
+
+            view.context.contentResolver.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.Images.ImageColumns.DISPLAY_NAME, name)
+                    put(MediaStore.Images.ImageColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.Images.ImageColumns.DATE_TAKEN, currentMillis)
+                    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        put(MediaStore.Images.ImageColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                    }
+                }
+
+                imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+                fos = imageUri?.let {
+                    resolver.openOutputStream(it)
+                }
+            }
+
+            fos?.use {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, it)
+            }
+
+            return if(imageUri == null) {
+                null
+            } else {
+                imageUri
+            }
+        }
+
+        fun getAbsolutePathFromImageUri(context: Context, uri: Uri): String? {
+            val cursor = context.contentResolver.query(uri, arrayOf(MediaStore.MediaColumns.DATA), null, null, null)
+            var absolutPath: String? = null
+            cursor?.use {
+                if(it.moveToFirst()) absolutPath = it.getString(0)
+            }
+            return absolutPath
+        }
+    }
+}
