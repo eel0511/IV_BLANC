91607391ca7477d555c0896b7920ffb10a7d6047
김민수 <ms001118@naver.com>
2022-02-09 20:36:14 +0900

    MO-471 StyleApi 스타일 등록 요청 함수 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/StyleApi.kt
@@ -0,0 +1,13 @@
+package com.strait.ivblanc.data.api
+
+import okhttp3.MultipartBody
+import retrofit2.Response
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface StyleApi {
+    @Multipart
+    @POST("/api/style/add")
+    suspend fun addStyle(@Part image: MultipartBody.Part, @Part styleDetails: MultipartBody.Part): Response<StyleResponse>
+}
