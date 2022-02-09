5acfba9fe3868eb6f15a3f3be4df2928da269ac7
김민수 <ms001118@naver.com>
2022-02-10 01:18:18 +0900

    MO-474 StyleApi 스타일 전체 요청 함수 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/StyleApi.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/api/StyleApi.kt
@@ -1,8 +1,10 @@
 package com.strait.ivblanc.data.api
 
+import com.strait.ivblanc.data.model.response.StyleAllResponse
 import com.strait.ivblanc.data.model.response.StyleResponse
 import okhttp3.MultipartBody
 import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -11,4 +13,7 @@ interface StyleApi {
     @Multipart
     @POST("/api/style/add")
     suspend fun addStyle(@Part image: MultipartBody.Part, @Part clothesList: MultipartBody.Part): Response<StyleResponse>
+
+    @GET("/api/style/finduserstyle")
+    suspend fun getAllStyles(): Response<StyleAllResponse>
 }
