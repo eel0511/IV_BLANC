a82db08d490f3929b62ee9424b10e2fce6353bd7
김민수 <ms001118@naver.com>
2022-02-10 09:34:10 +0900

    MO-475 StyleRepository 스타일 조회 요청 함수 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/StyleRepository.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/StyleRepository.kt
@@ -1,16 +1,20 @@
 package com.strait.ivblanc.data.repository
 
+import android.util.Log
 import com.strait.ivblanc.config.ApplicationClass
 import com.strait.ivblanc.data.api.StyleApi
+import com.strait.ivblanc.data.model.response.StyleAllResponse
 import com.strait.ivblanc.data.model.response.StyleResponse
 import com.strait.ivblanc.util.Resource
 import com.strait.ivblanc.util.StatusCode
 import okhttp3.MultipartBody
 import java.lang.Exception
 
+private const val TAG = "StyleRepository_debuk"
 class StyleRepository {
     val styleApi = ApplicationClass.sRetrofit.create(StyleApi::class.java)
 
+    // Multipart form data 요청
     suspend fun addClothes(image: MultipartBody.Part, clothesList: MultipartBody.Part): Resource<StyleResponse> {
         return try {
             val response = styleApi.addStyle(image, clothesList)
@@ -25,6 +29,24 @@ class StyleRepository {
             }
         } catch (e: Exception) {
             Resource.error(null, "네트워크 연결을 확인해 주세요")
+        }
+    }
+
+    suspend fun getAllStyles(): Resource<StyleAllResponse> {
+        return try {
+            val response = styleApi.getAllStyles()
+            if(response.isSuccessful) {
+                return if(response.code() == StatusCode.OK && response.body()!!.output == 1) {
+                    Resource.success(response.body()!!)
+                } else {
+                    Resource.error(response.body(), "등록된 스타일 조회를 할 수 없습니다.")
+                }
+            } else {
+                Resource.error(null, "알 수 없는 오류입니다.")
+            }
+        } catch (e: Exception) {
+            Log.d(TAG, "getAllStyles: error - ${e.message}")
+            Resource.error(null, "네트워크 연결을 확인해 주세요.")
         }
     }
 }
