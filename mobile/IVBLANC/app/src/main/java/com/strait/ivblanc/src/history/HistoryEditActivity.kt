7829289412abac4f1b5eb7416bd2e58dae6288be
김현수 <hskim9706@gmail.com>
2022-02-11 00:03:54 +0900

    MO-391 HistoryEditActivity 추가

---
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/history/HistoryEditActivity.kt
@@ -0,0 +1,12 @@
+package com.strait.ivblanc.src.history
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.strait.ivblanc.R
+
+class HistoryEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history_edit)
+    }
+}
