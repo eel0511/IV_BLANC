2f864a9bd46e9aa47819c38770d4d08e809c6da7
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-02-10 13:53:24 +0900

    FE-480 친구옷장 section 생성

--- web/src/pages/Friends/FriendsCloset.js
+++ web/src/pages/Friends/FriendsCloset.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import home from "../../assets/home.png";
+import styled from "styled-components";
+import Navbar from '../../components/Navbar';
+import ScrollToTop from '../../components/ScrollToTop';
 
 export default function FriendsCloset({ location }) {
   console.log(location)
 
   return(
-    <>
-    <h1>FriendsCloset</h1>
-    </>
+    <Section>
+      <ScrollToTop />
+      <div className='MyCloset__Nav'>
+        <Navbar />
+      </div>
+      <h1>FriendsCloset</h1>
+    </Section>
   );
 }
+
+const Section = styled.section`
+  // background-image: url(${home});
+  background-size: cover;
+  min-height: 100vh;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
+`;
