a34d049eeaf29fe6552330a84479a6d3ae3ecee3
이인섭 <inseob135@gmail.com>
2022-02-10 21:57:25 +0900

    FE-411 코드 수정

--- web/src/components/MyStyle/MyStyleAIModalBody.js
+++ web/src/components/MyStyle/MyStyleAIModalBody.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 function MyStyleAIModalBody() {
   const [selectedImg, setSelectedImg] = useState();
