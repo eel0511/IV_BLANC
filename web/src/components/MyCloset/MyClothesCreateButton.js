ba7a0fb90b8e5d9bd7138cee8d84f670a4f961e3
jyy6678 <jyy6678@naver.com>
2022-02-14 21:12:32 +0900

    옷등록 css

--- web/src/components/MyCloset/MyClothesCreateButton.js
+++ web/src/components/MyCloset/MyClothesCreateButton.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Modal } from 'react-bootstrap';
-import MyClothesCreateModalBody from './MyClothesCreateModalBody';
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import MyClothesCreateModalBody from "./MyClothesCreateModalBody";
 
 export default function MyClothesCreateButton({ getMyClothesData }) {
   const [show, setShow] = useState(false);
@@ -14,21 +14,43 @@ export default function MyClothesCreateButton({ getMyClothesData }) {
         type='button'
         className='btn btn-danger'
         style={{
-          'backgroundColor': '#ed6991',
-          color: 'white',
-          border: 'none',
+          backgroundColor: "#ed6991",
+          color: "white",
+          border: "none",
         }}
         onClick={handleShow}
       >
         등록 하기
       </button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>등록 하기</Modal.Title>
+      <Modal size='xl' show={show} onHide={handleClose}>
+        <Modal.Header
+          closeButton
+          style={{
+            marginLeft: "0%",
+            marginBottom: "0%",
+            backgroundColor: "#662d91",
+            color: "white",
+          }}
+        >
+          <div
+            className='d-flex align-content-center justify-content-center'
+            style={{
+              marginLeft: "45%",
+            }}
+          >
+            <Modal.Title
+              style={{
+                fontSize: '2rem',
+              }}
+            >등록 하기</Modal.Title>
+          </div>
         </Modal.Header>
         <Modal.Body>
-          <MyClothesCreateModalBody handleClose={handleClose} getMyClothesData={getMyClothesData} />
+          <MyClothesCreateModalBody
+            handleClose={handleClose}
+            getMyClothesData={getMyClothesData}
+          />
         </Modal.Body>
       </Modal>
     </>
--- web/src/components/MyCloset/MyClothesCreateModalBody.js
+++ web/src/components/MyCloset/MyClothesCreateModalBody.js
@@ -1,49 +1,20 @@
-import React, { useState } from 'react';
-import codeData from '../../codeData.json';
-import { Button } from 'react-bootstrap';
-import axios from 'axios';
-import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
-import { display } from '@mui/system';
+import React, { useState } from "react";
+import codeData from "../../codeData.json";
+import { Button, Row, Col } from "react-bootstrap";
+import axios from "axios";
+
 
 export default function MyClothesCreateModalBody({
   handleClose,
   getMyClothesData,
 }) {
-  const [selectedColor, setSelectedColor] = useState('#C14D49');
+  const [selectedColor, setSelectedColor] = useState("#C14D49");
   const [selectedMaterial, setSelectedMaterial] = useState(1);
-  const [selectedMainCategory, setSelectedMainCategory] = useState('상의');
+  const [selectedMainCategory, setSelectedMainCategory] = useState("상의");
   const [selectedSeason, setSelectedSeason] = useState(1);
   const [selectedSize, setSelectedSize] = useState();
   const [selectedSubCategory, setSelectedSubCategory] = useState();
   const [selectedImg, setSelectedImg] = useState();
-
-  const Label = styled.label`
-    display: inline-block;
-    width: 40px;
-    height: 40px;
-    border-radius: 20px;
-    border: none;
-    margin: 10px;
-    background-color: ${(props) => props.color};
-    border: 1px solid black;
-    h4 {
-      font-size: 0.8rem;
-      color: red;
-    }
-  `;
-
-  const ColorSelectorContainer = styled.div`
-    display: inline;
-    justify-content: space-between;
-    width: 500px;
-    margin-top: 8px;
-    padding: 10px;
-    /* input {
-    display: none;
-  } */
-    /* border: 1px solid salmon; */
-  `;
 
   const colorHandleChange = (e) => {
     setSelectedColor(e.target.value);
@@ -82,22 +53,22 @@ export default function MyClothesCreateModalBody({
 
   const createClothes = () => {
     const formData = new FormData();
-    formData.append('photo', selectedImg);
-    formData.append('category', Number(selectedSubCategory));
-    formData.append('color', selectedColor);
-    formData.append('material', Number(selectedMaterial));
-    formData.append('season', Number(selectedSeason));
-    formData.append('size', Number(selectedSize));
+    formData.append("photo", selectedImg);
+    formData.append("category", Number(selectedSubCategory));
+    formData.append("color", selectedColor);
+    formData.append("material", Number(selectedMaterial));
+    formData.append("season", Number(selectedSeason));
+    formData.append("size", Number(selectedSize));
 
     axios
-      .post('http://i6d104.p.ssafy.io:9999/api/clothes/add', formData, {
+      .post("http://i6d104.p.ssafy.io:9999/api/clothes/add", formData, {
         headers: {
-          'X-AUTH-TOKEN':
-            'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJQayI6IjEiLCJpYXQiOjE2NDM4Nzg4OTMsImV4cCI6MTY0NjQ3MDg5M30.Q2T5EQ38F53h1x037StKPwE-DBeqU0hBEAPY3D9w6WY',
+          "X-AUTH-TOKEN":
+            "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJQayI6IjEiLCJpYXQiOjE2NDM4Nzg4OTMsImV4cCI6MTY0NjQ3MDg5M30.Q2T5EQ38F53h1x037StKPwE-DBeqU0hBEAPY3D9w6WY",
         },
       })
       .then((response) => {
-        if (response.status === 200 && response.data.msg === '성공') {
+        if (response.status === 200 && response.data.msg === "성공") {
           alert(response.data.msg);
           handleClose();
           getMyClothesData();
@@ -112,170 +83,58 @@ export default function MyClothesCreateModalBody({
 
   return (
     <>
-      <h1>이미지 미리보기</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <div>
-                {selectedImg && (
-                  <img
-                    alt='sample'
-                    src={URL.createObjectURL(selectedImg)}
+      <Row>
+        <Col xs={6} md={6}>
+        <h4
+          // style={{
+          //   marginLeft: "45%",
+          // }}
+        >사진 등록</h4>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <div>
+                  {selectedImg && (
+                    <img
+                      alt='sample'
+                      src={URL.createObjectURL(selectedImg)}
+                      style={{
+                        // marginLeft: "20%",
+                        width: "200px",
+                        height: "200px",
+                      }}
+                    />
+                  )}
+                  <div
                     style={{
-                      margin: 'auto',
-                      padding: 'auto',
-                      width: '200px',
-                      height: '200px',
+                      alignItems: "center",
+                      justifyContent: "center",
+                      display: "flex",
                     }}
-                  />
-                )}
-                <div
-                  style={{
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    display: 'flex',
-                  }}
-                >
-                  <div>
-                    <h4>사진 등록</h4>
+                  >
+                    <input
+                      // style={{marginLeft: "20%",}}
+                      name='imgUpload'
+                      type='file'
+                      accept='image/*'
+                      onChange={imgHandleChange}
+                    />
                   </div>
-                  <input
-                    name='imgUpload'
-                    type='file'
-                    accept='image/*'
-                    onChange={imgHandleChange}
-                  />
-                  {/* <button 
-              style={{ backgroundColor: "gray", color: "white", width: "55px", height: "40px", cursor: "pointer", }} 
-              onClick={() => deleteFileImage()} > 삭제 </button>  */}
                 </div>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <hr />
-      <div className='create__color'>
-        <div className='col-3'>
-          <h2>색상</h2>
-        </div>
-        <div className='container row'>
-        {Object.entries(codeData['colors']).map((colorArray) => {
-          return (
-            <div key={colorArray[1]} className='col-3'>
-              <input
-                id={'색상' + colorArray[1]}
-                value={colorArray[1]}
-                className='form-check-input'
-                type='radio'
-                name='colorGroup'
-                defaultChecked={selectedColor === colorArray[1] ? true : false}
-                onChange={colorHandleChange}
-              />
-              <label
-                className='form-check-label create__colorLabel'
-                htmlFor={'색상' + colorArray[1]}
-                style={{ backgroundColor: colorArray[1]}}
-              >
-              </label>
-              {/* <Label 
-              id={'색상' + colorArray[1]}
-              value={colorArray[1]}
-              className='form-check-input'
-              type='radio'
-              name='colorGroup'
-              defaultChecked={selectedColor === colorArray[1] ? true : false}
-              onChange={colorHandleChange}
-              htmlFor={colorArray[0]} 
-              color={colorArray[1]} 
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        </Col>
 
-              >
-              <h4> {colorArray[0]} </h4>
-                </Label> */}
-            </div>
-          );
-        })}
-        </div>
-      </div>
-      <hr />
-      <div className='create__color'>
-        <div className='col-3'>
-          <h2>소재</h2>
-        </div>
-        <div className='container row'>
-          {Object.entries(codeData['material']).map((materialArray) => {
-            return (
-              <div key={materialArray[1]} className='col-3'>
-                <input
-                  id={'소재' + materialArray[0]}
-                  value={materialArray[1]}
-                  className='form-check-input'
-                  type='radio'
-                  name='materialGroup'
-                  defaultChecked={
-                    selectedMaterial === materialArray[1] ? true : false
-                  }
-                  onChange={materialHandleChange}
-                />
-                <label
-                  className='form-check-label'
-                  htmlFor={'소재' + materialArray[0]}
-                >
-                  {materialArray[0]}
-                </label>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <hr />
-      <div>
-        <div>
-          <h2>시즌</h2>
-        </div>
-        {Object.entries(codeData['season']).map((seasonArray) => {
-          return (
-            <React.Fragment key={seasonArray[1]}>
-              <input
-                id={'시즌' + seasonArray[0]}
-                value={seasonArray[1]}
-                className='form-check-input'
-                type='radio'
-                name='seasonGroup'
-                defaultChecked={selectedSeason === seasonArray[1] ? true : false}
-                onChange={seasonHandleChange}
-              />
-              <label
-                className='form-check-label'
-                htmlFor={'시즌' + seasonArray[0]}
-              >
-                {seasonArray[0]}
-              </label>
-            </React.Fragment>
-          );
-        })}
-      </div>
-      <hr />
-      <div>
-        <h2>사이즈</h2>
-      </div>
-      <React.Fragment>
-        <input
-          type={'number'}
-          placeholder='사이즈를 입력해주세요'
-          onChange={sizeHandleChange}
-        />
-      </React.Fragment>
-      <hr />
-      <div>
-        <h2>카테고리</h2>
-      </div>
-      {Object.entries(codeData['category']).map((mainCategoryArray, index) => {
+        <Col xs={6} md={6}>
+        <h4>카테고리</h4>
+      {Object.entries(codeData["category"]).map((mainCategoryArray, index) => {
         return (
           <React.Fragment key={index}>
             <input
-              id={'카테고리' + mainCategoryArray[0]}
+              id={"카테고리" + mainCategoryArray[0]}
               value={mainCategoryArray[0]}
               className='form-check-input'
               type='radio'
@@ -287,7 +146,7 @@ export default function MyClothesCreateModalBody({
             />
             <label
               className='form-check-label'
-              htmlFor={'카테고리' + mainCategoryArray[0]}
+              htmlFor={"카테고리" + mainCategoryArray[0]}
             >
               {mainCategoryArray[0]}
             </label>
@@ -295,7 +154,7 @@ export default function MyClothesCreateModalBody({
         );
       })}
       <div>
-        {Object.entries(codeData['category']).map(
+        {Object.entries(codeData["category"]).map(
           (mainCategoryArray, index) => {
             return (
               <React.Fragment key={index}>
@@ -305,7 +164,7 @@ export default function MyClothesCreateModalBody({
                       return (
                         <React.Fragment key={subCategoryArray[1]}>
                           <input
-                            id={'카테고리' + subCategoryArray[0]}
+                            id={"카테고리" + subCategoryArray[0]}
                             value={subCategoryArray[1]}
                             className='form-check-input'
                             type='radio'
@@ -314,7 +173,7 @@ export default function MyClothesCreateModalBody({
                           />
                           <label
                             className='form-check-label'
-                            htmlFor={'카테고리' + subCategoryArray[0]}
+                            htmlFor={"카테고리" + subCategoryArray[0]}
                           >
                             {subCategoryArray[0]}
                           </label>
@@ -327,16 +186,112 @@ export default function MyClothesCreateModalBody({
           }
         )}
       </div>
-      {/* <hr />
-      <div>
-        <h2>사진 등록</h2>
-      </div>
-      <input
-        type='file'
-        id='inputImg'
-        accept='image/*'
-        onChange={imgHandleChange}
-      ></input> */}
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col xs={4} md={4}>
+          <h4>색상</h4>
+          <div className='container row'>
+            {Object.entries(codeData["colors"]).map((colorArray) => {
+              return (
+                <div key={colorArray[1]} className='col-3 d-flex '>
+                  <input
+                    id={"색상" + colorArray[1]}
+                    value={colorArray[1]}
+                    className='form-check-input'
+                    type='radio'
+                    name='colorGroup'
+                    defaultChecked={
+                      selectedColor === colorArray[1] ? true : false
+                    }
+                    onChange={colorHandleChange}
+                  />
+                  <label
+                    className='form-check-label create__colorLabel'
+                    htmlFor={"색상" + colorArray[1]}
+                    style={{ backgroundColor: colorArray[1] }}
+                  ></label>
+                </div>
+              );
+            })}
+          </div>
+          <Row>
+            <Col xs={12} md={12}>
+            <h4>사이즈</h4>
+            <React.Fragment>
+        <input
+          type={"number"}
+          placeholder='사이즈를 입력해주세요'
+          onChange={sizeHandleChange}
+        />
+      </React.Fragment>
+
+            </Col>
+
+            <Col xs={12} md={12}>
+            <h4>시즌</h4>
+        {Object.entries(codeData["season"]).map((seasonArray) => {
+          return (
+            <React.Fragment key={seasonArray[1]}>
+              <input
+                id={"시즌" + seasonArray[0]}
+                value={seasonArray[1]}
+                className='form-check-input'
+                type='radio'
+                name='seasonGroup'
+                defaultChecked={
+                  selectedSeason === seasonArray[1] ? true : false
+                }
+                onChange={seasonHandleChange}
+              />
+              <label
+                className='form-check-label'
+                htmlFor={"시즌" + seasonArray[0]}
+              >
+                {seasonArray[0]}
+              </label>
+            </React.Fragment>
+          );
+        })}
+            </Col>
+          </Row>
+        </Col>
+
+        <Col xs={8} md={8}>
+          <h4>소재</h4>
+          <div className='container row'>
+            {Object.entries(codeData["material"]).map((materialArray) => {
+              const name = materialArray[0]
+              const val = name["id"]
+              return (
+                <div key={materialArray["val"]} className='col-3'>
+                  <input
+                    id={"소재" + materialArray["val"]}
+                    value={materialArray["val"]}
+                    className='form-check-input'
+                    type='radio'
+                    name='materialGroup'
+                    defaultChecked={
+                      selectedMaterial === materialArray["val"] ? true : false
+                    }
+                    onChange={materialHandleChange}
+                  />
+                  <label
+                    className='form-check-label'
+                    htmlFor={"소재" + materialArray["val"]}
+                    style={{ backgroundImage: `url({materialArray["src"]})` }}
+                  >
+                    {name}
+                  </label>
+                </div>
+              );
+            })}
+          </div>
+        </Col>
+      </Row>
+      <hr />
+
       <Button variant='secondary' onClick={createClothes}>
         등록
       </Button>
