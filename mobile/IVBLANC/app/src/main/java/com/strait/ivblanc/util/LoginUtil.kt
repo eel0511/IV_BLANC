863eded2c0c1639281b650452c5fa61e382f40cd
김민수 <ms001118@naver.com>
2022-02-14 11:55:28 +0900

    MO-525 LoginUtil 유저 정보 조회, 저장, 삭제 함수 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/util/LoginUtil.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/util/LoginUtil.kt
@@ -1,8 +1,13 @@
 package com.strait.ivblanc.util
 
 import com.strait.ivblanc.config.ApplicationClass
+import com.strait.ivblanc.data.model.dto.UserInfo
 
 object LoginUtil {
+    val USER_NAME = "userName"
+    val USER_EMAIL = "userEmail"
+    val USER_ID = "userId"
+
     private val preferences = ApplicationClass.sSharedPreferences
 
     fun isAutoLogin(): Boolean {
@@ -19,5 +24,29 @@ object LoginUtil {
 
     fun isTokenExisted(): Boolean {
         return !preferences.getString(ApplicationClass.JWT).isNullOrBlank()
+    }
+
+    fun saveUserInfo(userInfo: UserInfo) {
+        preferences.setString(USER_NAME, userInfo.name)
+        preferences.setString(USER_EMAIL, userInfo.email)
+        preferences.setString(USER_ID, userInfo.userId.toString())
+    }
+
+    fun deleteUserInfo() {
+        preferences.deleteString(USER_NAME)
+        preferences.deleteString(USER_EMAIL)
+        preferences.deleteString(USER_ID)
+    }
+
+    fun getUserInfo(): UserInfo? {
+        val userEmail = preferences.getString(USER_EMAIL)
+        val userName = preferences.getString(USER_NAME)
+        val userId = preferences.getString(USER_ID)?.toInt()
+
+        return if (userEmail.isNullOrBlank() or userName.isNullOrBlank() or (userId == null)) {
+            null
+        } else {
+            UserInfo(userEmail!!, userName!!, userId!!)
+        }
     }
 }
