56184ddecdb1698951259069e8da1d61c075ca21
SuhyungLee <eel0511@gmail.com>
2022-02-15 14:48:28 +0900

    MO-530

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/ui/PhotoListFragment.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/ui/PhotoListFragment.kt
@@ -174,13 +174,6 @@ class PhotoListFragment : BaseFragment<FragmentPhotoListBinding>(
     private fun setObserverLiveData() {
         clothesViewModel.clothesList.observe(this) {
             photoListRVAdapter.setDatas(it)
-
-            if(it.isEmpty()){
-                binding.noClothes.visibility=View.VISIBLE
-            }else{
-                binding.noClothes.visibility=View.GONE
-            }
-
         }
         clothesViewModel.recentlyAddedClothesList.observe(viewLifecycleOwner) {
             if (it.isNotEmpty()) {
