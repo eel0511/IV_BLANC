c75b72d14b21afe4f1fc9641bf64d8bed2174f36
이인섭 <inseob135@gmail.com>
2022-02-15 18:18:43 +0900

    FE-505 MyClothesCreateButton 스타일 수정

--- web/src/components/MyCloset/MyClothesCreateButton.js
+++ web/src/components/MyCloset/MyClothesCreateButton.js
@@ -14,7 +14,7 @@ export default function MyClothesCreateButton({ getMyClothesData }) {
         type='button'
         className='btn btn-danger'
         style={{
-          'backgroundColor': '#ed6991',
+          backgroundColor: '#ed6991',
           color: 'white',
           border: 'none',
         }}
@@ -23,12 +23,36 @@ export default function MyClothesCreateButton({ getMyClothesData }) {
         등록 하기
       </button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>등록 하기</Modal.Title>
+      <Modal show={show} size='xl' onHide={handleClose}>
+        <Modal.Header
+          closeButton
+          style={{
+            marginLeft: '0%',
+            marginBottom: '0%',
+            backgroundColor: '#662d91',
+            color: 'white',
+          }}
+        >
+          <div
+            className='d-flex align-content-center justify-content-center'
+            style={{
+              marginLeft: '45%',
+            }}
+          >
+            <Modal.Title
+              style={{
+                fontSize: '2rem',
+              }}
+            >
+              등록 하기
+            </Modal.Title>
+          </div>
         </Modal.Header>
         <Modal.Body>
-          <MyClothesCreateModalBody handleClose={handleClose} getMyClothesData={getMyClothesData} />
+          <MyClothesCreateModalBody
+            handleClose={handleClose}
+            getMyClothesData={getMyClothesData}
+          />
         </Modal.Body>
       </Modal>
     </>
