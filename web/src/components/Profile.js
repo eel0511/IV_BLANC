f50257918f90a913a73d442b9b1e28f304c11052
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-02-15 11:29:14 +0900

    FE-494 프로필 컴포넌트 생성

---
+++ web/src/components/Profile.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { BsPersonCircle } from 'react-icons/bs';
+import { Menu } from '@mui/material';
+
+export default function Profile() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className='Profile'>
+      <BsPersonCircle
+        className='Profile__icon'
+        id='basic-button'
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup='true'
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      />
+      <Menu
+        id='basic-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        로그아웃
+      </Menu>
+
+    </div>
+  );
+}
