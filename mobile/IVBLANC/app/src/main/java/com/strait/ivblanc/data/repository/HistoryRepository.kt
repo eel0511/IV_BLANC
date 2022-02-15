ab7c2ab67db65f3039bb6b699e17304829727652
김현수 <hskim9706@gmail.com>
2022-02-15 16:17:55 +0900

    MO-391 HistoryRepositry update 함수 추가

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/HistoryRepository.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/data/repository/HistoryRepository.kt
@@ -4,6 +4,8 @@ import com.strait.ivblanc.config.ApplicationClass
 import com.strait.ivblanc.data.api.HistoryApi
 import com.strait.ivblanc.data.model.response.HistoryResponse
 import com.strait.ivblanc.util.Resource
+import okhttp3.MultipartBody
+import retrofit2.http.Part
 import java.lang.Exception
 
 private const val TAG = "HistoryRepository_debug"
@@ -48,6 +50,26 @@ class HistoryRepository {
     suspend fun getHistoryByMonth(year: Int, month: Int): Resource<HistoryResponse> {
         return try {
             val response = historyApi.getHistoryByMonth(year, month)
+            if(response.isSuccessful) {
+                return if(response.code() == 200 && response.body()!!.output == 1 && response.body()!!.dataSet?.isNotEmpty() == true) {
+                    Resource.success(response.body()!!)
+                } else {
+                    Resource.error(response.body(), "히스토리 데이터가 없습니다.")
+                }
+            } else {
+                Resource.error(null, "알 수 없는 오류입니다.")
+            }
+        } catch (e: Exception) {
+            val msg = e.message
+            Resource.error(null, "네트워크 상태를 확인해 주세요.")
+        }
+    }
+
+    suspend fun updateHistory(historyId: MultipartBody.Part, location: MultipartBody.Part, field: MultipartBody.Part, date: MultipartBody.Part, weather: MultipartBody.Part,
+                              temperature_low: MultipartBody.Part, temperature_high: MultipartBody.Part, text: MultipartBody.Part, subject: MultipartBody.Part,
+                              styleId: MultipartBody.Part): Resource<HistoryResponse> {
+        return try{
+            val response = historyApi.updateHistory(historyId, location, field, date, weather, temperature_low, temperature_high, text, subject, styleId)
             if(response.isSuccessful) {
                 return if(response.code() == 200 && response.body()!!.output == 1 && response.body()!!.dataSet?.isNotEmpty() == true) {
                     Resource.success(response.body()!!)
