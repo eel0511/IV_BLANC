6b8be87a129041294f26a25a7095253ab5e7c153
김현수 <hskim9706@gmail.com>
2022-02-15 12:24:44 +0900

    MO-391 StyleSelectActivity 코드 정리

--- mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/history/StyleSelectActivity.kt
+++ mobile/IVBLANC/app/src/main/java/com/strait/ivblanc/src/history/StyleSelectActivity.kt
@@ -1,25 +1,18 @@
 package com.strait.ivblanc.src.history
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.strait.ivblanc.R
-import com.strait.ivblanc.adapter.ExpandableRecyclerViewAdapter
 import com.strait.ivblanc.adapter.StyleSelectRecyclerViewAdapter
 import com.strait.ivblanc.config.BaseActivity
-import com.strait.ivblanc.data.model.dto.PhotoItem
 import com.strait.ivblanc.data.model.dto.Style
 import com.strait.ivblanc.data.model.viewmodel.StyleViewModel
 import com.strait.ivblanc.databinding.ActivityStyleSelectBinding
 
 private const val TAG = "StyleSelect"
 class StyleSelectActivity : BaseActivity<ActivityStyleSelectBinding>(ActivityStyleSelectBinding::inflate) {
-//    lateinit var exAdapter: ExpandableRecyclerViewAdapter<Style>
     private lateinit var styleSelectRVA: StyleSelectRecyclerViewAdapter
     private val styleViewModel: StyleViewModel by viewModels()
 
@@ -36,17 +29,13 @@ class StyleSelectActivity : BaseActivity<ActivityStyleSelectBinding>(ActivitySty
     }
 
     private fun setRecyclerView(){
-
-
         styleSelectRVA = StyleSelectRecyclerViewAdapter()
 
         styleViewModel.getAllStyles()
 
-        binding.recyclerViewStyleList.adapter = styleSelectRVA
         binding.recyclerViewStyleList.apply {
             adapter = styleSelectRVA
             layoutManager = GridLayoutManager(this@StyleSelectActivity, 3, RecyclerView.VERTICAL, false)
-
         }
 
         styleSelectRVA.itemClickListener = object : StyleSelectRecyclerViewAdapter.ItemClickListener{
