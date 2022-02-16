2f1ed45ad38b7d9133a02f40c7eea5e502538a7a
jyy6678 <jyy6678@naver.com>
2022-02-16 14:39:22 +0900

    등록창 css 수정

--- web/src/components/MyCloset/MyClothesCreateModalBody.js
+++ web/src/components/MyCloset/MyClothesCreateModalBody.js
@@ -87,10 +87,10 @@ export default function MyClothesCreateModalBody({
         <Col xs={5} md={5}>
           <h4
             style={{
-              backgroundColor: '#662d91',
+              
               marginLeft: '36%',
               marginRight: '40%',
-              color: 'white',
+              color: '#EA8FAA',
               borderRadius: '15px',
             }}
           >
@@ -137,10 +137,10 @@ export default function MyClothesCreateModalBody({
         <Col xs={7} md={7}>
           <h4
             style={{
-              backgroundColor: '#662d91',
+              
               marginLeft: '37%',
               marginRight: '47%',
-              color: 'white',
+              color: '#EA8FAA',
               borderRadius: '15px',
             }}
           >
@@ -205,10 +205,10 @@ export default function MyClothesCreateModalBody({
         <Col xs={5} md={5}>
           <h4
             style={{
-              backgroundColor: '#662d91',
-              marginLeft: '40%',
-              marginRight: '47%',
-              color: 'white',
+              
+              marginLeft: '41%',
+              marginRight: '48%',
+              color: '#EA8FAA',
               borderRadius: '15px',
             }}
           >
@@ -245,10 +245,9 @@ export default function MyClothesCreateModalBody({
             <Col xs={12} md={12}>
               <h4
                 style={{
-                  backgroundColor: '#662d91',
-                  marginLeft: '38%',
+                  marginLeft: '39%',
                   marginRight: '44%',
-                  color: 'white',
+                  color: '#EA8FAA',
                   borderRadius: '15px',
                   marginTop: '2%',
                 }}
@@ -272,11 +271,11 @@ export default function MyClothesCreateModalBody({
             <Col xs={12} md={12}>
               <h4
                 style={{
-                  backgroundColor: '#662d91',
+                  
                   marginLeft: '40%',
                   marginTop: '2%',
-                  marginRight: '47%',
-                  color: 'white',
+                  marginRight: '48%',
+                  color: '#EA8FAA',
                   borderRadius: '15px',
                 }}
               >
@@ -315,10 +314,10 @@ export default function MyClothesCreateModalBody({
         <Col xs={7} md={7}>
           <h4
             style={{
-              backgroundColor: '#662d91',
+              
               marginLeft: '40%',
-              marginRight: '51%',
-              color: 'white',
+              marginRight: '52%',
+              color: '#EA8FAA',
               borderRadius: '15px',
             }}
           >
@@ -375,7 +374,7 @@ export default function MyClothesCreateModalBody({
 
       <Button
         style={{
-          backgroundColor: '#662d91',
+          backgroundColor: "#EA8FAA",
           fontSize: '1.2rem',
           marginLeft: '85%',
           color: 'white',
