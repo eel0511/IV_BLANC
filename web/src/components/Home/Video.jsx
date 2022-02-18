036b75eda27b198ee6236ddd8e0341a241f74aec
bangbbang <87457532+bangbbang@users.noreply.github.com>
2022-02-18 10:33:32 +0900

    FE UCC url 수정

--- web/src/components/Home/Video.jsx
+++ web/src/components/Home/Video.jsx
@@ -25,7 +25,7 @@ function Video() {
         <iframe
           width="960"
           height="515"
-          src="https://www.youtube.com/embed/BoZFs1-k5A4"
+          src="https://www.youtube.com/embed/qyCiQDBnMlo"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
